7ad5cf426fa7623ca89febc544e1bed91dc7dd00
Steve Harris <steve@harrislineage.com>
2022-10-11 13:15:47 -0500

    Update spacepreview.js
    Refactored spacePreviews to use jquery.hoverDelay and persistent previews. Corrected other minor errors.

--- features/spacepreview/spacepreview.js
+++ features/spacepreview/spacepreview.js
@@ -1,28 +1,37 @@
-chrome.storage.sync.get('spacePreviews', function (result) {
-
-    if (result.spacePreviews == true) {
-        $('a[href*="Space:"]').hover(function (e) { // jquery.hover() handlerIn (show spacePreview)
-            $(this).attr('id', 'spaceHover');
-            var sPreview = document.createElement('div');
-            sPreview.setAttribute('id', 'spacePreview');
-            sPreview.setAttribute('class', 'box rounded');
-            sPreview.setAttribute('style', `z-index:999; height: 450px; overflow: scroll; position:absolute;`);
-            document.getElementById(this.id).appendChild(sPreview);
-            $.ajax({
-                url: this.href,
-                context: document.body
-            }).done(function (result) {
-                var pageContent = $(result).find('.ten.columns').html();
-                try {
-                    document.getElementById('spacePreview').innerHTML = pageContent;
-                } catch { }
-            });
-        },
-            // jqeury.hover() handlerOut (remove spacePreview)
-            function () {
-                $('#spacePreview').remove();
-                $(this).attr('id', '');
-            }
-        )
+chrome.storage.sync.get("spacePreviews", function (result) {
+  if (result.spacePreviews == true) {
+    $('.ten.columns a[href*="/wiki/Space:"]').hoverDelay({
+      delayIn: 1000,
+      delayOut: 0,
+      handlerIn: function ($element) {
+        console.log($element[0].href);
+        $element.attr("id", "spaceHover");
+        var sPreview = document.createElement("div");
+        sPreview.setAttribute("id", "spacePreview");
+        sPreview.setAttribute("class", "box rounded");
+        sPreview.setAttribute(
+          "style",
+          `z-index:9999; max-height:450px; overflow: scroll; position:absolute; padding: 10px; margin-top:-20px;`
+        );
+        document.getElementById("spaceHover").parentElement.appendChild(sPreview);
+        $.ajax({
+          url: $element[0].href,
+          context: document.body,
+        }).done(function (result) {
+          var pageContent = $(result).find(".ten.columns").html();
+          try {
+            document.getElementById(
+              "spacePreview"
+            ).innerHTML = `<span style="float:right; font-weight:700;">click outside to close</span>${pageContent}`;
+          } catch {}
+        });
+      },
+    });
+  }
+  $(document).on("click", function (event) {
+    if ($(event.target).closest("#spacePreview").length === 0) {
+      $("#spacePreview").remove();
+      $("#spaceHover").attr("id", "");
     }
-})
+  });
+});
