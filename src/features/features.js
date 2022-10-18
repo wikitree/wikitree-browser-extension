9a2b23a3333aa1c2ca4769aa80f992c9c4e5457e
Przemek Więch <pwiech@gmail.com>
2022-10-18 23:27:13 +0200

    Change the way features are registered

---
+++ src/features/features.js
@@ -0,0 +1,16 @@
+import "./agc/agc_content";
+import "./akaNameLinks/akaNameLinks";
+import "./appsMenu/appsMenu";
+import "./collapsibleDescendantsTree/collapsibleDescendantsTree";
+import "./darkMode/darkMode";
+import "./distanceAndRelationship/distanceAndRelationship";
+import "./draftList/draftList";
+import "./familyGroup/familyGroup";
+import "./familyTimeline/familyTimeline";
+import "./locationsHelper/locationsHelper";
+import "./printerfriendly/printerfriendly";
+import "./randomProfile/randomProfile";
+import "./sourcepreview/sourcepreview";
+import "./spacepreview/spacepreview";
+import "./wt+/contentEdit";
+import "./bioCheck/bioCheck";
