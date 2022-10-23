19c083421ec29e722c4399ca4b8c044b135cb3a5
Ian Beacall <ian@shogenapps.com>
2022-10-23 21:46:56 +0900

    Update collapsibleDescendantsTree.js
    Small fix.

--- src/features/collapsibleDescendantsTree/collapsibleDescendantsTree.js
+++ src/features/collapsibleDescendantsTree/collapsibleDescendantsTree.js
@@ -1,29 +1,30 @@
-import $ from 'jquery';
-import {pageProfile} from '../../core/common';
-import './collapsibleDescendantsTree.css';
+import $ from "jquery";
+import { pageProfile } from "../../core/common";
+import "./collapsibleDescendantsTree.css";
 
-chrome.storage.sync.get('collapsibleDescendantsTree', (result) => {
-	if (result.collapsibleDescendantsTree && pageProfile == true) { 
-
+chrome.storage.sync.get("collapsibleDescendantsTree", (result) => {
+  if (result.collapsibleDescendantsTree && pageProfile == true) {
     // Look out for the appearance of new list items in the descendantsContainer
     const descendantsObserver = new MutationObserver(function (mutations_list) {
       mutations_list.forEach(function (mutation) {
-      mutation.addedNodes.forEach(function (added_node) {
-        if (added_node.tagName == "OL") {
-        theLIS = $(added_node).find("li");
-        theLIS.each(function (index, thing) {
-            setTimeout(function () {
-            createDescendantsButton(index, thing);
+        mutation.addedNodes.forEach(function (added_node) {
+          if (added_node.tagName == "OL") {
+            const theLIS = $(added_node).find("li");
+            theLIS.each(function (index, thing) {
+              setTimeout(function () {
+                createDescendantsButton(index, thing);
               }, 10);
-            })
+            });
           }
         });
       });
     });
 
     if ($("#descendantsContainer").length) {
-        descendantsObserver.observe(document.querySelector("#descendantsContainer"),
-        { subtree: true, childList: true });
+      descendantsObserver.observe(
+        document.querySelector("#descendantsContainer"),
+        { subtree: true, childList: true }
+      );
     }
 
     // Add buttons
@@ -33,27 +34,27 @@ chrome.storage.sync.get('collapsibleDescendantsTree', (result) => {
           setTimeout(function () {
             createDescendantsButton(index, thing);
           }, 10);
-        })
+        });
       }
     }
 
     async function createDescendantsButton(n, li) {
-    // Attach class to avoid adding button more than once
-      if (li.classList.contains('collapse')) {
+      // Attach class to avoid adding button more than once
+      if (li.classList.contains("collapse")) {
         return;
       }
       if (!isNextSiblingDiv(li)) {
         return;
       }
-      $(li).addClass('collapse');
+      $(li).addClass("collapse");
       const button = $("<button class='wikitreeturbo'>-</button>");
       $(button).click(toggleCollapse);
       $(li).prepend(button);
     }
 
     function isNextSiblingDiv(el) {
-      if(el.nextElementSibling) {
-        if (el.nextElementSibling.tagName=="DIV") {
+      if (el.nextElementSibling) {
+        if (el.nextElementSibling.tagName == "DIV") {
           return true;
         }
       }
@@ -62,9 +63,8 @@ chrome.storage.sync.get('collapsibleDescendantsTree', (result) => {
 
     function toggleCollapse(e) {
       const s = $(e.target).text();
-      $(e.target).text(s=='-' ? '+' : '-');
+      $(e.target).text(s == "-" ? "+" : "-");
       $(e.target.parentElement.nextElementSibling).toggle();
     }
-    
   }
 });
