98f826712b606eb6709c24679091187d6de86043
Ian Beacall <ian@shogenapps.com>
2022-10-24 14:26:17 +0900

    Changed Category Finder Pins icon

--- src/features/categoryFinderPins/categoryFinderPins.js
+++ src/features/categoryFinderPins/categoryFinderPins.js
@@ -9,7 +9,9 @@ async function addCategoryLinksToDropdown() {
           const pin = $(
             "<span class='autocomplete-suggestion-maplink'><a target='_new' href='https://www.wikitree.com/wiki/Category:" +
               term +
-              "'><img src='/images/icons/map.gif'></a></span>"
+              "'><img src='" +
+              chrome.runtime.getURL("images/newTab.png") +
+              "'></a></span>"
           );
           if ($(this).prev("span").length == 0) {
             pin.insertBefore($(this));
