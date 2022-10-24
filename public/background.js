19faf921dc13e85328cc49133199bc53a889b7d5
Ian Beacall <ian@shogenapps.com>
2022-10-24 15:56:43 +0900

    Update background.js
    Added update action (open the update page);
    Added an install action (open the extension page)

--- public/background.js
+++ public/background.js
@@ -1,5 +1,13 @@
 chrome.runtime.onInstalled.addListener(function (details) {
   if (details.reason == "install") {
     chrome.runtime.openOptionsPage();
+    window.open(
+      "https://www.wikitree.com/wiki/Space:WikiTree_Browser_Extension"
+    );
+  }
+  if (details.reason == "update") {
+    chrome.runtime.openOptionsPage();
+    // Remove the slashes below after a significant update (e.g. a new feature).
+    // window.open("https://www.wikitree.com/wiki/Space:WikiTree_Browser_Extension_Update");
   }
 });
