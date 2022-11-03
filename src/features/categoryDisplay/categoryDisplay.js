9d20e14ba4281c7d116dfa6ff374eb22dba8d8b3
Steve Harris <steve@harrislineage.com>
2022-11-03 09:36:24 -0500

    Update categoryDisplay.js

--- src/features/categoryDisplay/categoryDisplay.js
+++ src/features/categoryDisplay/categoryDisplay.js
@@ -1,16 +1,19 @@
 import $ from 'jquery';
 import {pageProfile} from '../../core/common';
-import { checkIfFeatureEnabled } from "../../core/options/options_storage.js";
-
-function moveCategories(){
-    $("#categories").find('span[class="SMALL"]').remove();
-    $('div:contains("This page has been accessed")').next().attr('id','cousinLinks');
-    $("#cousinLinks").after($("#categories"));
-    $("#categories").css({"border":"none","padding":"5px","margin-top":"10px"});
-}
+import { checkIfFeatureEnabled, getFeatureOptions } from "../../core/options/options_storage";
 
 checkIfFeatureEnabled("categoryDisplay").then((result) => {
     if (result && pageProfile == true) { 
         moveCategories();
     }
 })
+
+async function moveCategories(){
+    const options = await getFeatureOptions("categoryDisplay");
+    console.log(options)
+
+    $("#categories").find('span[class="SMALL"]').remove();
+    $('div:contains("This page has been accessed")').next().attr('id','cousinLinks');
+    $("#cousinLinks").after($("#categories"));
+    $("#categories").css({"border":"none","padding":"5px","margin-top":"10px"});
+}
