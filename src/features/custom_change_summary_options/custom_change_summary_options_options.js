7693171e1b073c5bd53c91b1764abbaf84557e71
Ian Beacall <ian@shogenapps.com>
2022-11-18 07:00:07 +0900

    work in progress

---
+++ src/features/custom_change_summary_options/custom_change_summary_options_options.js
@@ -0,0 +1,22 @@
+import { registerFeature, OptionType } from "../../core/options/options_registry";
+
+const customChangeSummaryOptions = {
+  name: "Custom Change Summary Options",
+  id: "customChangeSummaryOptions",
+  description: "Add your own options to the change summary options on edit pages.",
+  category: "Editing",
+  defaultValue: true,
+};
+
+registerFeature(customChangeSummaryOptions);
+
+/*
+  options: [
+    {
+      id: "myFirstOption",
+      type: OptionType.CHECKBOX,
+      label: "Enables my first option",
+      defaultValue: true,
+    },
+  ],
+*/
