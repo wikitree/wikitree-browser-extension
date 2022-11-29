852c3182d55fab62f375b03b6111f4a6224f4bc2
Ian Beacall <ian@shogenapps.com>
2022-11-30 06:18:51 +0900

    Access keys
    Adds access keys. g: G2G Recent Activity; r: Random Profile, n: Nav Home Page; h: Help Search; s: Save; e: Edit; k: Category; p: Preview. (<a href='https://en.wikipedia.org/wiki/Access_key'>More details</a>)

---
+++ src/features/access_keys/access_keys.js
@@ -0,0 +1,37 @@
+import $ from "jquery";
+import { checkIfFeatureEnabled, getFeatureOptions } from "../../core/options/options_storage";
+
+checkIfFeatureEnabled("accessKeys").then((result) => {
+  if (result) {
+    addAccessKeys();
+  }
+});
+
+function addAccessKeys() {
+  setTimeout(function () {
+    if ($("#previewButton").length) {
+      $("#previewButton")[0].accessKey = "p";
+    }
+
+    $("body").append("<a style='display:none;' id='G2Grecent' href='https://www.wikitree.com/g2g/activity'></a>");
+    $("#G2Grecent")[0].accessKey = "g";
+    if ($("a[title='Edit Profile and Family Relationships'],a[title='Edit this Profile']").length) {
+      $("a[title='Edit Profile and Family Relationships'],a[title='Edit this Profile']")[0].accessKey = "e";
+    }
+    if ($("#wpSave").length) {
+      $("#wpSave")[0].accessKey = "s";
+    }
+    if ($("#addCategoryButton").length) {
+      $("#addCategoryButton")[0].accessKey = "k";
+    }
+    if ($("a.pureCssMenui.randomProfile").length) {
+      $("a.pureCssMenui.randomProfile")[0].accessKey = "r";
+    }
+    if ($("a[href$='/wiki/Special:Home']").length) {
+      $("a[href$='/wiki/Special:Home']")[0].accessKey = "n";
+    }
+    if ($("a[href$='/wiki/Special:SearchPages']").length) {
+      $("a[href$='/wiki/Special:SearchPages']")[0].accessKey = "h";
+    }
+  }, 1000);
+}
