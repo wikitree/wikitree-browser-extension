0ab3a919d268f1b1644f853a2094bd4f7703d227
Ian Beacall <ian@shogenapps.com>
2022-12-05 17:40:34 +0900

    Make moving save box optional

--- src/features/custom_change_summary_options/custom_change_summary_options_options.js
+++ src/features/custom_change_summary_options/custom_change_summary_options_options.js
@@ -6,6 +6,14 @@ const customChangeSummaryOptions = {
   description: "Add your own phrases to the change summary phrases on edit pages.",
   category: "Editing",
   defaultValue: true,
+  options: [
+    {
+      id: "movingSaveBox",
+      type: OptionType.CHECKBOX,
+      label: "Show the options and save button in the right-hand column",
+      defaultValue: true,
+    },
+  ],
 };
 
 registerFeature(customChangeSummaryOptions);
