98872683f17be9cee66d833b3abf0b20451b4dbd
Ian Beacall <ian@shogenapps.com>
2022-12-06 21:28:16 +0900

    Add sticky toolbar to Project, Category, and Space pages

--- src/features/sticky_toolbar/sticky_toolbar.js
+++ src/features/sticky_toolbar/sticky_toolbar.js
@@ -3,7 +3,11 @@ import "./sticky_toolbar.css";
 import { checkIfFeatureEnabled } from "../../core/options/options_storage";
 
 checkIfFeatureEnabled("stickyToolbar").then((result) => {
-  if (result && $("body.page-Special_EditPerson").length) {
+  if (
+    result &&
+    ($("body.page-Special_EditPerson").length ||
+      (window.location.href.match(/Project:|Category:|Space:/) && $("#toolbar").length))
+  ) {
     setTimeout(function () {
       $("#editToolbarExt").appendTo($("#toolbar"));
     }, 5000);
