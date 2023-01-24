9f5d022b6e835f37fb2a238504b0d590db9bd92b
Ian Beacall <ian@shogenapps.com>
2023-01-25 04:33:20 +0900

    Work in progress

--- src/content.js
+++ src/content.js
@@ -6,6 +6,7 @@ import "./features/access_keys/access_keys";
 import "./features/agc/agc_content";
 import "./features/akaNameLinks/akaNameLinks";
 import "./features/appsMenu/appsMenu";
+import "./features/auto_bio/auto_bio";
 import "./features/bioCheck/bioCheck";
 import "./features/categoryDisplay/categoryDisplay";
 import "./features/categoryFinderPins/categoryFinderPins";
