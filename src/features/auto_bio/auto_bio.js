3159ad979967aeb344c44541af69806d7b93f5ce
Ian Beacall <ian@shogenapps.com>
2023-02-04 04:06:16 +0900

    Update auto_bio.js
    in progress

--- src/features/auto_bio/auto_bio.js
+++ src/features/auto_bio/auto_bio.js
@@ -3,6 +3,7 @@ import * as cheerio from "cheerio";
 //import "./my_feature.css";
 import { getPeople } from "../dna_table/dna_table";
 import { PersonName } from "./person_name.js";
+import { isOK } from "../../core/common";
 import { checkIfFeatureEnabled, getFeatureOptions } from "../../core/options/options_storage";
 
 // Function to parse the wikitables
@@ -71,25 +72,98 @@ function formatDates(person) {
   return `(${birthDate.substring(0, 4)}–${deathDate.substring(0, 4)})`;
 }
 
-function formatDate(dateStr) {
-  const date = new Date(dateStr);
-  if (isNaN(date.getTime())) {
-    return "";
+function formatDate(date, status = "on") {
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  if (!date) return "";
+
+  let [year, month, day] = date.split("-");
+  year = parseInt(year);
+  month = parseInt(month);
+  day = parseInt(day);
+
+  if (month === 0 && day === 0) {
+    return status === "before" ? `before ${year}` : `about ${year}`;
   }
-  const options = { year: "numeric", month: "long", day: "numeric" };
-  return date.toLocaleDateString("en-US", options);
+  if (day === 0) {
+    return status === "before" ? `before ${months[month - 1]} ${year}` : `about ${months[month - 1]} ${year}`;
+  }
+  if (isNaN(day)) {
+    const split = date.split(" ");
+    split.forEach(function (bit) {
+      if (bit.match(/[0-9]{4}/)) {
+        year = bit;
+      } else if (bit.match(/[a-z]/i)) {
+        months.forEach(function (aMonth, index) {
+          if (aMonth.match(bit));
+          month = index - 1;
+        });
+      } else {
+        day = bit;
+      }
+    });
+  }
+  let dateString = `${
+    status === "before"
+      ? "before"
+      : status === "after"
+      ? "after"
+      : status === "guess"
+      ? "about"
+      : !status
+      ? day
+        ? "on"
+        : "in"
+      : ""
+  } ${day ? `${months[month - 1]} ${day},` : months[month - 1]} ${year}`;
+
+  return dateString.trim();
+}
+
+function nameLink(person) {
+  return "[[" + person.Name + "|" + person.FullName + "]]";
 }
 
 function buildBirth(person) {
-  let text = person.FullName + " was born";
+  let text = "'''" + person.FullName + "'''" + " was born";
   if (person.BirthLocation) {
     text += " in " + person.BirthLocation;
+    let birthPlaces = person.BirthLocation.split(",");
+    birthPlaces.forEach(function (place) {
+      window.usedPlaces.push(place.trim());
+    });
   }
   if (person.BirthDate) {
-    text += " on " + formatDate(person.BirthDate);
+    text += " " + formatDate(person.BirthDate, person.mStatus_BirthDate || "");
   }
   if (person.Parents) {
     text += buildParents(person);
+  }
+  text += ".";
+  return text;
+}
+
+function buildDeath(person) {
+  let text = person.FirstName + " died";
+  if (person.DeathDate) {
+    text += " " + formatDate(person.DeathDate, person.mStatus_DeathDate || "");
+  }
+  if (person.DeathLocation) {
+    let place = minimalPlace(person.DeathLocation);
+    text += " in " + place;
   }
   text += ".";
   return text;
@@ -104,12 +178,11 @@ function buildParents(person) {
   }
   let parents = person.Parents;
   if (parents) {
-    let wanted = ["FullName"];
     if (person.Father) {
       let father = person.Parents[person.Father];
       let aName = new PersonName(father);
-      let fatherName = aName.withParts(wanted);
-      text += fatherName;
+      father.FullName = aName.withParts(["FullName"]);
+      text += nameLink(father);
       text += " " + formatDates(father);
     }
     if (person.Father && person.Mother) {
@@ -118,41 +191,97 @@ function buildParents(person) {
     if (person.Mother) {
       let mother = person.Parents[person.Mother];
       let aName = new PersonName(mother);
-      let motherName = aName.withParts(wanted);
-      text += motherName;
+      mother.FullName = aName.withParts(["FullName"]);
+      text += nameLink(mother);
       text += " " + formatDates(mother);
     }
   }
   return text;
 }
 
-function buildSpouses(person) {
-  let text = "";
-  let spouseKeys = Object.keys(person.Spouses);
-  if (person.Spouses) {
-    spouseKeys.forEach(function (aSpouse) {
-      text += person.FirstName + " married " + spouse.Name;
+function minimalPlace(place) {
+  const placeSplit = place.split(",");
+  let showPlace = [];
+  let used = 0;
+  placeSplit.forEach(function (place, index) {
+    place = place.trim();
+    if (window.usedPlaces.includes(place)) {
+      used++;
+    }
+    if (index == 0) {
+      showPlace.push(place);
+    } else if (!window.usedPlaces.includes(place) || used < 2) {
+      showPlace.push(place);
+      window.usedPlaces.push(place);
+    } else {
+      return showPlace.join(", ");
+    }
+  });
+  return showPlace.join(", ");
+}
 
-      if (spouse.BirthDate) {
-        text += " (born " + spouse.BirthDate + ")";
+function buildSpouses(person) {
+  let spouseKeys = Object.keys(person.Spouses);
+  let marriages = [];
+  if (person.Spouses) {
+    spouseKeys.forEach(function (key) {
+      let text = "";
+      let spouse = person.Spouses[key];
+      let spouseName = new PersonName(spouse);
+      spouse.FullName = spouseName.withParts(["FirstNames", "LastNameAtBirth"]);
+      text += person.FirstName + " married " + nameLink(spouse);
+      if (isOK(spouse.BirthDate) || spouse.BirthLocation) {
+        text += " (born";
       }
-      if (spouse.DeathDate) {
-        text += ", who died " + spouse.DeathDate;
+      if (isOK(spouse.BirthDate)) {
+        text += " " + formatDate(spouse.BirthDate, spouse.DataStatus.BirthDate);
       }
-      text += ". ";
-      if (spouse.Parents) {
-        text +=
-          "The parents of " +
-          spouse.Name +
-          " were " +
-          spouse.Parents.father.Name +
-          " and " +
-          spouse.Parents.mother.Name +
-          ".";
+      if (spouse.BirthLocation) {
+        let place = minimalPlace(spouse.BirthLocation);
+        text += " in " + place;
       }
+      text += "; ";
+      if (spouse.Father || spouse.Mother) {
+        text += spouse.Gender == "Male" ? "son" : spouse.Gender == "Female" ? "daughter" : "child";
+        text += " of ";
+        if (spouse.Father) {
+          let spouseFather = window.biographySpouseParents[0].people[spouse.Id].Parents[spouse.Father];
+          let spouseFatherName = new PersonName(spouseFather);
+          spouseFather.FullName = spouseFatherName.withParts(["FullName"]);
+          text += spouseFather.FullName;
+          if (spouseFather.BirthDate) {
+            text += " " + formatDates(spouseFather);
+          }
+        }
+        if (spouse.Father && spouse.Mother) {
+          text += " and ";
+        }
+        if (spouse.Mother) {
+          let spouseMother = window.biographySpouseParents[0].people[spouse.Id].Parents[spouse.Mother];
+          let spouseMotherName = new PersonName(spouseMother);
+          spouseMother.FullName = spouseMotherName.withParts(["FirstNames", "LastNameAtBirth"]);
+          text += spouseMother.FullName;
+          if (spouseMother.BirthDate) {
+            text += " " + formatDates(spouseMother);
+          }
+        }
+      }
+      if (spouse.BirthDate || spouse.BirthLocation) {
+        text += ")";
+      }
+      if (spouse.marriage_date) {
+        let dateStatus = spouse.data_status.marriage_date;
+        text += " " + formatDate(spouse.marriage_date, dateStatus);
+      }
+      if (spouse.marriage_location) {
+        let place = minimalPlace(spouse.marriage_location);
+        text += " in " + place;
+      }
+      text += ".";
+      marriages.push(text);
     });
   }
-  return text;
+  return marriages;
 }
 
 function buildCensuses(text) {
@@ -362,6 +491,7 @@ function addCitations(text) {
 }
 
 async function generate() {
+  window.usedPlaces = [];
   let spouseLinks = $("span[itemprop='spouse'] a");
   let profileID = $("a.pureCssMenui0 span.person").text();
   let keys = profileID;
@@ -372,9 +502,14 @@ async function generate() {
     }
   });
   console.log(keys);
-  let people = await getPeople(keys, 0, 0, 0, 1, 1, "*");
-  console.log(people);
-  let person = people[0].people[people[0].resultByKey[profileID].Id];
+  window.biographyPeople = await getPeople(keys, 0, 0, 0, 1, 1, "*");
+  console.log(window.biographyPeople);
+  let person = window.biographyPeople[0].people[window.biographyPeople[0].resultByKey[profileID].Id];
+  if (person.Spouses) {
+    let spouseKeys = Object.keys(person.Spouses);
+    window.biographySpouseParents = await getPeople(spouseKeys.join(","), 0, 0, 0, 0, 0, "*");
+  }
+  console.log(window.biographySpouseParents);
   console.log(person);
   let profilePerson = getFormData();
   let personKeys = Object.keys(profilePerson);
@@ -382,13 +517,23 @@ async function generate() {
     person[aKey] = profilePerson[aKey];
   });
   console.log(person);
-  let text = "==Biography==\n";
+
   let wanted = ["FullName"];
   let aName = new PersonName(person);
   person.FullName = aName.withParts(wanted);
+  person.FirstName = aName.withParts(["FirstName"]);
   person.Pronouns = getPronouns(person);
-  text += buildBirth(person) + buildSpouses(person);
-  text = addCitations(text);
+
+  // Output
+  let text = "==Biography==\n";
+  text += buildBirth(person) + "\n\n";
+  let marriages = buildSpouses(person);
+  marriages.forEach(function (aMarriage) {
+    text += aMarriage + "\n\n";
+  });
+  text += buildDeath(person) + "\n\n";
+
+  //text = addCitations(text);
   /*
   let bioTextArea = document.getElementById("biotext");
   bioTextArea.value = text + bioTextArea.value;
