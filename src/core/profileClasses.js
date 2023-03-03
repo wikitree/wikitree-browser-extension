8b86389139f4be2ae5bdf70b6a2a2401802f2e4f
Jonathan Duke <jonathanduke@users.noreply.github.com>
2023-03-03 11:30:30 -0600

    additional tweaks to profileClasses and readingMode to include other profile elements

--- src/core/profileClasses.js
+++ src/core/profileClasses.js
@@ -4,7 +4,7 @@ import { pageProfile, pageCategory, pageSpace, isEditPage } from './common';
 export let hasProfileClasses = false;
 
 export function canTweakProfile() {
-    return (!isEditPage && (pageProfile || pageCategory || pageSpace));
+  return (!isEditPage && (pageProfile || pageCategory || pageSpace));
 }
 
 export function ensureProfileClasses() {
@@ -37,6 +37,9 @@ export function ensureProfileClasses() {
       // mark the content section (on the left of the sidebar) which contains the biography, sources, etc. up to where the comments section starts; for categories, the content is all in the root section
       $(".x-profile > .ten.columns, .x-profile-category").first().addClass("x-content");
 
+      // mark alert boxes (like research notes, orphaned profile, etc.)
+      $("x-content > .status, .x-content > a[name]:last-of-type ~ .box.orange, .x-content:not(* > a[name]) > .box.orange").addClass("x-alert");
+
       // mark the sidebar to the right (with DNA connections, images, collaboration, etc.)
       $(".x-profile > .six.columns").first().addClass("x-sidebar");
 
@@ -50,15 +53,21 @@ export function ensureProfileClasses() {
 
       // mark the audit lines in the profile that show the manager, last modified, how many times the page has been accessed, etc.
       $(".x-content > div.SMALL").addClass("x-audit");
-      $(".x-content p.SMALL").filter(function() { var txt = $(this).text(); return (txt.indexOf('last modified') > -1 && txt.indexOf('been accessed') > -1); }).addClass("x-audit"); // category pages are displayed this way
+      $(".x-content p.SMALL").filter(function () { var txt = $(this).text(); return (txt.indexOf('last modified') > -1 && txt.indexOf('been accessed') > -1); }).addClass("x-audit"); // category pages are displayed this way
+
+      // mark the sources link in the table of contents
+      $("#toc a[href='#Sources']").closest("li").addClass("x-toc-sources");
 
       // mark stickers inside the content
       $(".x-content > div").filter(function () { return $(this).css("float") == "right" && $(this).css("display") == "flex"; }).addClass("x-sticker");
 
+      // mark inline citations (both <ref> tags and "citation needed")
+      $(".x-content sup.reference, .x-content sup > i > a[href$='/Help:Sources']").closest("sup").addClass("x-citation");
+
       // mark inline images
       $(".x-content table").has("a.image > img").addClass("x-inline-img");
 
-      // mark inline tables
+      // mark inline tables (inline images must be marked first so that they will be excluded, along with the table of contents)
       $(".x-content table:not(#toc):not(.x-inline-img)").addClass("x-inline-table");
 
       // mark root sections in content (h2 only)
@@ -84,7 +93,7 @@ export function ensureProfileClasses() {
       $(".x-profile-person a[name='matches']").parent().addClass("x-merges").nextAll(".five").addClass("x-merges");
 
       // mark connections to famous people
-      $(".x-profile-person > div:last-child").filter(function() { return $(this).text().indexOf('degrees from') > -1 && $(this).has("a[href~='Special:Connect']"); }).last().addClass("x-connections");
+      $(".x-profile-person > div:last-child").filter(function () { return $(this).text().indexOf('degrees from') > -1 && $(this).has("a[href~='Special:Connect']"); }).last().addClass("x-connections");
 
       // mark the container for the categories box, including the breadcrumbs at the bottom of profiles (ie. S > Smith > John Smith)
       $("#categories").closest(".container").addClass("x-categories");
--- src/features/debugProfileClasses/debugProfileClasses.js
+++ src/features/debugProfileClasses/debugProfileClasses.js
@@ -1,11 +1,8 @@
-import $ from "jquery";
-import { pageProfile, pageCategory, pageSpace, isEditPage } from '../../core/common';
 import { ensureProfileClasses, canTweakProfile } from "../../core/profileClasses";
 import { checkIfFeatureEnabled } from "../../core/options/options_storage.js";
 
 checkIfFeatureEnabled("debugProfileClasses").then((result) => {
-  if (result && canTweakProfile())
-  {
+  if (result && canTweakProfile()) {
     import("./debugProfileClasses.css");
     ensureProfileClasses();
   }
