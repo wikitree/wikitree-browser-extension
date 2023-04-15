069a2f34aff0608455795c9512b11f8355d59d1d
Jonathan Duke <jonathanduke@users.noreply.github.com>
2023-04-15 09:44:36 -0500

    updated Sticky Header to work on G2G; added a delay to properly scroll to anchor in URL hash

--- src/features/sticky_header/sticky_header.js
+++ src/features/sticky_header/sticky_header.js
@@ -6,11 +6,16 @@ import $ from "jquery";
 import { checkIfFeatureEnabled } from "../../core/options/options_storage";
 
 async function initStickyHeader() {
-  let headerHeight = $("#header").css("height");
+  let headerHeight = $("#header, .qa-header").first().css("height");
   if (headerHeight) {
     document.documentElement.style.setProperty("--header-height", headerHeight);
     if (!$("body").is(".home")) {
       $("html").addClass("sticky-header");
+      if (window.location.hash) {
+        let hash = window.location.hash;
+        window.location.hash = "";
+        window.location.hash = hash;
+      }
     }
   }
 }
@@ -18,6 +23,7 @@ async function initStickyHeader() {
 checkIfFeatureEnabled("stickyHeader").then((result) => {
   if (result) {
     import("./sticky_header.css");
-    initStickyHeader();
+    // wait a second for other items to adjust the page layout
+    window.setTimeout(initStickyHeader, 1000);
   }
 });
