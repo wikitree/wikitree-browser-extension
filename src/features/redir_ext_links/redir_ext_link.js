253bc563f132081accecdaad8f7d943dd8c5dd29
Rob Pavey <robpavey@gmail.com>
2023-04-21 16:03:15 -0700

    redir_ext_links: update for new Ancestry affiliate links

--- src/features/redir_ext_links/redir_ext_link.js
+++ src/features/redir_ext_links/redir_ext_link.js
@@ -75,6 +75,83 @@ function updateAncestryLink(linkText, options) {
   return newLink;
 }
 
+function updateAncestryRakutenLink(link, options) {
+  if (options.ancestryDomain == "none" || !options.ancestryDomain) {
+    return linkText;
+  }
+
+  // NOTE: Ancestry switched from Partnerize to Rakuten on 18 Apr 2023. Partnerize used the
+  // "prf.hn" format, Rakuten uses this new click.linksynergy format.
+  // And example link in this format:
+  // https://click.linksynergy.com/deeplink?id=Xib7NfnK11s&amp;mid=50138&amp;murl=https%3A%2F%2Fsearch.ancestry.com%2Fcgi-bin%2Fsse.dll%3Findiv%3D1%26db%3D61596%26h%3D8900762
+  let linkStartText = "https%3A%2F%2Fsearch.ancestry";
+  let realLinkIndex = link.indexOf(linkStartText);
+  if (realLinkIndex == -1) {
+    linkStartText = "https%3A%2F%2Fwww.ancestry";
+    realLinkIndex = link.indexOf(linkStartText);
+  }
+  //console.log("openAncestryLink, realLinkIndex is: " + realLinkIndex);
+  if (realLinkIndex != -1) {
+    let domainEndIndex = link.indexOf("%2F", realLinkIndex + linkStartText.length);
+    if (domainEndIndex != -1) {
+      let linkStart = link.substring(realLinkIndex, domainEndIndex);
+      // linkStart = https%3A%2F%2Fsearch.ancestry.com for example
+      let ancestryStartIndex = linkStart.indexOf("ancestry");
+      if (ancestryStartIndex != -1) {
+        let domain = linkStart.substring(ancestryStartIndex);
+        //console.log("openAncestryLink, domain is: " + domain);
+
+        let desiredDomain = options.ancestryDomain;
+        //console.log("openAncestryLink, desiredDomain is: " + desiredDomain);
+
+        if (desiredDomain != domain) {
+          // we want to change the link, first decide if we are changing to a domain supported by
+          // the Rakuten links
+          const rukutenSupportedDomains = [
+            { domain: "ancestry.com", mid: "50138" },
+            { domain: "ancestry.co.uk", mid: "50140" },
+            { domain: "ancestry.ca", mid: "50139" },
+            { domain: "ancestry.com.au", mid: "50142" },
+          ];
+          let mid = "";
+          for (let entry of rukutenSupportedDomains) {
+            if (entry.domain == desiredDomain) {
+              mid = entry.mid;
+              break;
+            }
+          }
+          if (mid) {
+            // it is a supported domain to modify the mid and domain in link
+            let newLink = link.replace(domain, desiredDomain);
+            if (newLink && newLink != link) {
+              link = newLink;
+              //console.log("openAncestryLink, new link is: " + newLink);
+            }
+            newLink = link.replace(/([&;])mid=\d+/, "$1mid=" + mid);
+            if (newLink && newLink != link) {
+              link = newLink;
+              //console.log("openAncestryLink, new link is: " + newLink);
+            }
+          } else {
+            // not one of the supported domains - change to a non-referral link
+            let encodedPlainLink = link.substring(realLinkIndex);
+            //console.log("openAncestryLink, encodedPlainLink is: " + encodedPlainLink);
+            let plainLink = decodeURIComponent(encodedPlainLink);
+            //console.log("openAncestryLink, plainLink is: " + plainLink);
+            let newLink = plainLink.replace(domain, desiredDomain);
+            if (newLink && newLink != plainLink) {
+              link = newLink;
+              //console.log("openAncestryLink, new link is: " + newLink);
+            }
+          }
+        }
+      }
+    }
+  }
+
+  return link;
+}
+
 function updateFmpLink(linkText, options) {
   if (options.fmpDomain == "none") {
     return linkText;
@@ -93,11 +170,11 @@ function updateFmpLink(linkText, options) {
 function updateLink(linkText, options) {
   let newLink = linkText;
 
-  if (newLink.search(/\:\/\/[^\.]+\.ancestry[\.\l]/) != -1) {
+  if (newLink.search(/\:\/\/[^\.]+\.linksynergy\./) != -1 && newLink.search(/\.ancestry[\.l]/) != -1) {
+    newLink = updateAncestryRakutenLink(newLink, options);
+  } else if (newLink.search(/\:\/\/[^\.]+\.ancestry[\.\l]/) != -1) {
     newLink = updateAncestryLink(newLink, options);
-  }
-
-  if (newLink.search(/\:\/\/[^\.]+\.findmypast\./) != -1) {
+  } else if (newLink.search(/\:\/\/[^\.]+\.findmypast\./) != -1) {
     newLink = updateFmpLink(newLink, options);
   }
 
