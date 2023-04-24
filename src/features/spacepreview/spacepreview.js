bc951c75b4109d44ba53fc9c055092cf602c28c3
Jonathan Duke <jonathanduke@users.noreply.github.com>
2023-04-24 12:20:00 -0500

    cleaned up space preview and updated to call the getProfile API

--- src/features/spacepreview/spacepreview.js
+++ src/features/spacepreview/spacepreview.js
@@ -1,5 +1,6 @@
 /*
 Created By: Steve Harris (Harris-5439)
+Contributors: Jonathan Duke (Duke-5773)
 */
 
 import $ from "jquery";
@@ -8,40 +9,68 @@ import { checkIfFeatureEnabled } from "../../core/options/options_storage";
 
 checkIfFeatureEnabled("spacePreviews").then((result) => {
   if (result) {
-    $('.ten.columns a[href*="/wiki/Space:"], .sixteen.columns a[href*="/wiki/Space:"]').hoverDelay({
+    $('.columns a[href*="/wiki/Space:"]').hoverDelay({
       delayIn: 1000,
       delayOut: 0,
       handlerIn: function ($element) {
         $("#spacePreview").remove();
-        $("#spaceHover").attr("id", "");
-        console.log($element[0].href);
-        $element.attr("id", "spaceHover");
-        var sPreview = document.createElement("div");
-        sPreview.setAttribute("id", "spacePreview");
-        sPreview.setAttribute("class", "box rounded");
-        sPreview.setAttribute(
-          "style",
-          `z-index:9999; max-height:450px; overflow: scroll; position:absolute; padding: 10px; font-style: normal; font-weight: normal; text-decoration: none;`
+        let $popup = $(
+          '<div id="spacePreview" class="box rounded"' +
+            ' style="display: none; z-index:9999; max-height:450px; overflow: scroll; position:absolute; padding: 10px; font-style: normal; font-weight: normal; text-decoration: none;"' +
+            ">Loading...</div>"
         );
-        document.getElementById("spaceHover").parentElement.appendChild(sPreview);
-        $.ajax({
-          url: $element[0].href,
-          context: document.body,
-        }).done(function (result) {
-          var pageContent = $(result).find(".ten.columns").html();
+
+        const targetSpaceId = decodeURIComponent($element[0].href.match(/\/wiki\/(Space:.*?)(#.*|$)/i)[1]);
+        let hasRedirect = false;
+        function populateSpacePreview(spaceId) {
+          // this can be recursive since getProfile does not handle redirects on space pages
           try {
-            document.getElementById(
-              "spacePreview"
-            ).innerHTML = `<span style="float:right; font-weight:700;">click outside to close</span>${pageContent}`;
-          } catch {}
-        });
+            $.ajax({
+              url: "https://api.wikitree.com/api.php",
+              type: "POST",
+              dataType: "json",
+              data: {
+                action: "getProfile",
+                key: spaceId,
+                fields: "Bio",
+                bioFormat: "both",
+                resolveRedirect: 0,
+              },
+              xhrFields: { withCredentials: true },
+            }).done(function (results) {
+              let bio = null;
+              if (results && results.length && results[0] && results[0].profile && (bio = results[0].profile.bioHTML)) {
+                let redirectMatch;
+                if ((redirectMatch = results[0].profile.bio.match(/^\s*#REDIRECT \[\[\s*(Space:.*)\s*\]\]\s*$/))) {
+                  hasRedirect = true;
+                  populateSpacePreview(redirectMatch[1]);
+                } else {
+                  $popup.get(0).innerHTML =
+                    (hasRedirect
+                      ? `<div style="color: #c00; font-size: small; font-weight: bold;">[[${targetSpaceId}]] redirected to [[${results[0].page_name}]]</div>`
+                      : "") + bio;
+                  $popup.fadeIn("fast");
+                }
+              }
+            });
+          } catch (err) {
+            console.warn(err);
+            $("#spacePreview").remove();
+          }
+        }
+
+        populateSpacePreview(targetSpaceId);
+        $element.after($popup);
       },
     });
+
+    // intercept clicks outside of the preview to close it
+    $(document).on("click", function (event) {
+      if ($(event.target).closest("#spacePreview").length === 0) {
+        $("#spacePreview").fadeOut("fast", function () {
+          $(this).remove();
+        });
+      }
+    });
   }
-  $(document).on("click", function (event) {
-    if ($(event.target).closest("#spacePreview").length === 0) {
-      $("#spacePreview").remove();
-      $("#spaceHover").attr("id", "");
-    }
-  });
 });
