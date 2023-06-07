f15ae5596ddc0bf479c4a5972d02a8cdb5c626de
Ian Beacall <ian@shogenapps.com>
2023-06-07 23:06:37 +0900

    Add functions to reload content scripts on update

--- public/background.js
+++ public/background.js
@@ -1,3 +1,42 @@
+async function executeScript(tabId, script, callback) {
+  chrome.scripting.executeScript(
+    {
+      target: { tabId: tabId },
+      files: [script],
+    },
+    callback
+  );
+}
+
+function reloadContentScripts() {
+  let manifest = chrome.runtime.getManifest();
+
+  let contentScripts = manifest.content_scripts;
+
+  for (let contentScript of contentScripts) {
+    let queryObj = {
+      url: contentScript.matches,
+    };
+
+    chrome.tabs.query(queryObj, function (tabs) {
+      for (let tab of tabs) {
+        if (tab.status == "complete") {
+          for (let script of contentScript.js) {
+            executeScript(tab.id, script, () => {
+              const lastErr = chrome.runtime.lastError;
+              if (lastErr) {
+                console.log("tab: " + tab.id + " lastError: " + JSON.stringify(lastErr));
+              } else {
+                console.log("tab: " + tab.id + " OK. script is " + script);
+              }
+            });
+          }
+        }
+      }
+    });
+  }
+}
+
 if (chrome.runtime) {
   chrome.runtime.onInstalled.addListener(function (details) {
     if (details.reason == "install") {
@@ -7,6 +46,7 @@ if (chrome.runtime) {
       });
       chrome.runtime.openOptionsPage();
     } else if (details.reason == "update") {
+      reloadContentScripts();
       chrome.storage.sync.get("wbeSettings_disableUpdateNotification").then((result) => {
         if (!result.wbeSettings_disableUpdateNotification) {
           // Use this to open the extension update page on update. Comment it out the rest of the time.
