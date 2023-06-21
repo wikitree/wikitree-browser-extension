aa838227aa2af4bf0ce24a5d6db3f0263ac829e8
Ian Beacall <ian@shogenapps.com>
2023-06-21 18:57:40 +0900

    Update/Change Image Zoom
    Hover over an image. The image zoom feature is activated when you hover over an image. You'll see a "+" sign inside a circle appear on the bottom right corner of the image.
    
    Click the "+". Click the "+" sign to zoom in on the image. The image will expand to a larger size, and the rest of the page will darken to focus on the zoomed image.
    
    Move the image. You can click and drag the zoomed image around the screen to reposition it.
    
    Zoom in and out on the zoomed image. Use your mouse wheel to further zoom in or out on the zoomed image. If you don't have a mouse wheel, click on the zoomed image to zoom in.
    
    Close the zoomed image. You can close the zoomed image in two ways:
    
    Double click on the zoomed image.
    Click anywhere on the darkened part of the page.
    Remember, the zoomed image will reset back to its original size and position when you move your mouse away from the image and then hover over it again.

--- src/features/image_zoom/image_zoom.js
+++ src/features/image_zoom/image_zoom.js
@@ -8,102 +8,153 @@ shouldInitializeFeature("imageZoom").then((result) => {
   }
 });
 
+function wheelZoomHandler(e) {
+  e.preventDefault();
+  const delta = Math.sign(e.originalEvent.deltaY);
+  let scaleFactor;
+  if (delta < 0) {
+    scaleFactor = 1.1;
+  } else {
+    scaleFactor = 0.9;
+  }
+
+  let scale = $(this).data("scale") || 1;
+  scale = scale * scaleFactor;
+  $(this).css("transform", `scale(${scale})`);
+  $(this).data("scale", scale);
+}
+
+function createZoomedImage(src, alt) {
+  const imgElement = $("<img>", {
+    src: src,
+    alt: alt,
+    class: "zoom-image",
+    css: {
+      position: "absolute",
+      width: "auto",
+      maxWidth: "100%",
+      maxHeight: "100%",
+      boxShadow: "5px 5px 15px rgba(0,0,0,0.3)",
+      zIndex: 20001,
+      border: "2px solid white",
+    },
+  });
+  imgElement.appendTo("body");
+  imgElement.css({
+    top: ($(window).height() - imgElement.height()) / 2 + $(window).scrollTop(),
+    left: ($(window).width() - imgElement.width()) / 2,
+  });
+  imgElement.draggable();
+  imgElement.dblclick(function () {
+    $(".dark-screen").remove();
+    imgElement.remove();
+  });
+  imgElement.on("wheel", wheelZoomHandler);
+  imgElement.on("click", function () {
+    let scale = $(this).data("scale") || 1;
+    scale = scale * 1.1; // Choose your scale factor, 1.1 as an example
+    $(this).css("transform", `scale(${scale})`);
+    $(this).data("scale", scale);
+  });
+  setupDarkScreen(imgElement);
+  return imgElement;
+}
+
+function setupDarkScreen(zoomedImage) {
+  let darkScreen = $("<div>").appendTo("body");
+  darkScreen.css({
+    position: "fixed",
+    top: 0,
+    left: 0,
+    height: "100%",
+    width: "100%",
+    backgroundColor: "rgba(0,0,0,0.5)",
+    zIndex: 20000,
+  });
+  darkScreen.addClass("dark-screen");
+  darkScreen.on("click", function () {
+    if (zoomedImage) {
+      zoomedImage.remove();
+    }
+    $(".dark-screen").remove();
+  });
+}
+
 function setupImageZoom() {
-  let hoverTimer; // timer for hover delay
+  let hoverTimer;
   let zoomedImage = null;
   let originalPosition = {};
+  let mouseOutTimer = null;
 
-  // Separate event delegation for images with the class "scale-with-grid"
-  $(document).on("mouseover", "img.scale-with-grid", function (e) {
-    if (this.src) {
-      originalPosition = $(this).offset();
-      $(this).addClass("zoomable");
-      $(this).on("wheel", function (e) {
-        e.preventDefault();
-        const delta = Math.sign(e.originalEvent.deltaY);
-        let scaleFactor;
-        if (delta < 0) {
-          scaleFactor = 1.1;
-        } else {
-          scaleFactor = 0.9;
+  $(document).on(
+    "mouseover",
+    "a:has(img.scale-with-grid), a:has(img[src*='thumb']:not(.commenter-image))",
+    function (e) {
+      clearTimeout(mouseOutTimer);
+      let img = $(this).find("img");
+      let imgSrc = img.attr("src");
+      let imgAlt = img.attr("alt");
+
+      // Check if an overlay already exists for this image
+      if (img.parent().find(".image_zoom_overlay").length > 0) {
+        return; // If it does, exit the function
+      }
+
+      if (imgSrc) {
+        if (imgSrc.includes("thumb")) {
+          imgSrc = imgSrc.replace("/thumb/", "/").replace(/\/[^/]+$/, "");
         }
 
-        let scale = $(this).data("scale") || 1;
-        scale = scale * scaleFactor;
-        $(this).css("transform", `scale(${scale})`);
-        $(this).data("scale", scale);
-      });
-      $(this).draggable();
-    }
-  });
+        originalPosition = img.offset();
+        let parent = $(this).css({ display: "inline-block", position: "relative" });
+        const overlay = $('<div class="image_zoom_overlay">+</div>').appendTo(parent);
 
-  $(document).on("mouseout", "img.scale-with-grid", function () {
-    $(this).off("wheel"); // remove wheel event
-    $(this).css("transform", `scale(1)`); // reset scaling
-    $(this).data("scale", 1); // reset scale data
-    // Check if the image is draggable before destroying it
-    if ($(this).data("ui-draggable")) {
-      $(this).draggable("destroy");
-    }
-    $(this).offset(originalPosition);
-    $(this).removeClass("zoomable");
-  });
+        overlay.css({ "z-index": 99999, position: "absolute", bottom: "0", right: "0" });
 
-  // Separate event delegation for images with "thumb" in their src
-  $(document).on("mouseover", "img[src*='thumb']:not(.commenter-image)", function (e) {
-    const src = $(this).attr("src");
-    const alt = $(this).attr("alt");
-    if (
-      src &&
-      !($(".x-privacy img[title*='Privacy Level: Private']").length && $(this).closest(".x-thumbnail").length)
-    ) {
-      const newSrc = src.replace("/thumb/", "/").replace(/\/[^/]+$/, "");
-      hoverTimer = setTimeout(function () {
-        setupDarkScreen();
-        const imgElement = $("<img>", {
-          src: newSrc,
-          alt: alt,
-          class: "zoom-image",
-          css: {
-            position: "absolute",
-            width: "400px",
-            boxShadow: "5px 5px 15px rgba(0,0,0,0.3)", // shadow
-            zIndex: 20001,
-            border: "2px solid white",
-          },
-        });
-        imgElement.appendTo("body");
-        imgElement.css({
-          top: ($(window).height() - imgElement.height()) / 2 + $(window).scrollTop(),
-          left: ($(window).width() - imgElement.width()) / 2,
-        });
-        imgElement.draggable();
-        imgElement.dblclick(function () {
-          $(".dark-screen").remove();
-          imgElement.remove();
-        });
-        imgElement.on("wheel", function (e) {
+        overlay.one("pointerdown", (e) => {
+          console.log("overlay clicked");
           e.preventDefault();
-          const delta = Math.sign(e.originalEvent.deltaY);
-          let scaleFactor;
-          if (delta < 0) {
-            scaleFactor = 1.1;
-          } else {
-            scaleFactor = 0.9;
-          }
-
-          let scale = $(this).data("scale") || 1;
-          scale = scale * scaleFactor;
-          $(this).css("transform", `scale(${scale})`);
-          $(this).data("scale", scale);
+          e.stopPropagation();
+          createZoomedImage(imgSrc, imgAlt);
+          overlay.hide();
+          return false;
         });
-        zoomedImage = imgElement;
-      }, 1500); // 1.5 second delay before showing the image
-    }
-  });
 
-  $(document).on("mouseout", "img[src*='thumb']:not(.commenter-image)", function () {
-    clearTimeout(hoverTimer);
+        overlay.on("mouseover", (e) => {
+          $(this)
+            .closest("a")
+            .on("click", function (e) {
+              e.preventDefault();
+            });
+        });
+
+        overlay.on("mouseout", (e) => {
+          $(this).closest("a").off("click");
+        });
+
+        img.addClass("zoomable");
+        img.on("wheel", wheelZoomHandler);
+        img.draggable();
+        overlay.show();
+      }
+    }
+  );
+
+  $(document).on("mouseout", "a:has(img.scale-with-grid), a:has(img[src*='thumb']:not(.commenter-image))", function () {
+    mouseOutTimer = setTimeout(() => {
+      let img = $(this).find("img");
+      img.off("wheel");
+      img.css("transform", `scale(1)`);
+      img.data("scale", 1);
+      if (img.data("ui-draggable")) {
+        img.draggable("destroy");
+      }
+      img.offset(originalPosition);
+      img.css({ position: "static", top: "auto", left: "auto" });
+      img.removeClass("zoomable");
+      $(this).css({ display: "", position: "" });
+      $(this).find(".image_zoom_overlay").remove();
+    }, 500);
   });
 
   $(document).on("click", function (e) {
@@ -112,18 +163,4 @@ function setupImageZoom() {
     zoomedImage.remove();
     zoomedImage = null;
   });
-
-  function setupDarkScreen() {
-    let darkScreen = $("<div>").appendTo("body");
-    darkScreen.css({
-      position: "fixed",
-      top: 0,
-      left: 0,
-      height: "100%",
-      width: "100%",
-      backgroundColor: "rgba(0,0,0,0.5)",
-      zIndex: 20000,
-    });
-    darkScreen.addClass("dark-screen");
-  }
 }
