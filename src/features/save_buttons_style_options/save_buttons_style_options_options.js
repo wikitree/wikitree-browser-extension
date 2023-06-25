7f699c80597176182cb7c732c8ab8d2e2c84c477
Ian Beacall <ian@shogenapps.com>
2023-06-25 15:29:32 +0900

    SaveButtonsStyleOptions
    Turns the Compare and Return links in the Save section on the Edit page into buttons.
    Gives options for big or small buttons.

---
+++ src/features/save_buttons_style_options/save_buttons_style_options_options.js
@@ -0,0 +1,30 @@
+/*
+Created By: Ian Beacall (Beacall-6)
+*/
+
+import { registerFeature, OptionType } from "../../core/options/options_registry";
+import { isProfileEdit } from "../../core/pageType";
+
+registerFeature({
+  name: "Save Buttons Style Options",
+  id: "saveButtonsStyleOptions",
+  description: "Turns the Compare and Return links in the Save section on the Edit page into buttons.",
+  category: "Editing/Edit_Profile",
+  creators: [{ name: "Ian Beacall", wikitreeid: "Beacall-6" }],
+  contributors: [],
+  defaultValue: false,
+  pages: [isProfileEdit],
+  options: [
+    {
+      id: "buttonSize",
+      type: OptionType.RADIO,
+      label: "Button Size",
+      values: [
+        { value: "allSmall", text: "All Small" },
+        { value: "halfSmall", text: "Small Compare and Return Buttons" },
+        { value: "large", text: "All Large" },
+      ],
+      defaultValue: "helfSmall",
+    },
+  ],
+});
