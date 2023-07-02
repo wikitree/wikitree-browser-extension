5db09688311dcf1d889683457428ec26ffce6981
Ian Beacall <ian@shogenapps.com>
2023-07-03 01:52:52 +0900

    Update auto_bio.js

--- src/features/auto_bio/auto_bio.js
+++ src/features/auto_bio/auto_bio.js
@@ -260,7 +260,7 @@ function fixLocations() {
         if (country.name == lastLocationBit) {
           let aNote;
           if (window.autoBioOptions?.nativeNames) {
-            if (country.name != country.nativeName && country.name != "Ireland") {
+            if (country.name != country.nativeName && !["Ireland", "Northern Ireland"].includes(country.name)) {
               if (locationBits.length == 1) {
                 event.Location = country.nativeName;
               } else {
@@ -268,7 +268,7 @@ function fixLocations() {
               }
             }
           } else {
-            if (country.name != country.nativeName && country.name != "Ireland") {
+            if (country.name != country.nativeName && !["Ireland", "Northern Ireland"].includes(country.name)) {
               aNote =
                 "The native name for the country of " +
                 event.Event +
