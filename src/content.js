fec7e82f170a87dc00dde4e2d85e373c46115405
Ian Beacall <ian@shogenapps.com>
2023-10-05 23:30:21 +0900

    Update content.js

--- src/content.js
+++ src/content.js
@@ -82,6 +82,7 @@ import "./features/randomProfile/randomProfile";
 import "./features/what_links_here/what_links_here";
 import "./features/category_management/category_management";
 import "./features/category_tables/category_tables";
+import "./features/editor_expander/editor_expander";
 
 /*
  * debugging features for development only
