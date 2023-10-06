ec13c0c2d0af2316b03bc51e2a13f20680c56b25
Ian Beacall <ian@shogenapps.com>
2023-10-06 20:59:26 +0900

    Update category_tables_options.js

--- src/features/category_tables/category_tables_options.js
+++ src/features/category_tables/category_tables_options.js
@@ -6,9 +6,10 @@ import { registerFeature, OptionType } from "../../core/options/options_registry
 import { isCategoryPage } from "../../core/pageType";
 
 registerFeature({
-  name: "Category Tables",
+  name: "Category Table",
   id: "categoryTables",
-  description: "Adds a button to Category pages to display all of the people in the category in a table.",
+  description:
+    "Adds a button to Category pages to display all of the people on a category page in a sortable and filterable table.",
   category: "Other",
   creators: [{ name: "Ian Beacall", wikitreeid: "Beacall-6" }],
   contributors: [],
