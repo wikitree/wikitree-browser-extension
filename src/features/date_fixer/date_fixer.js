86f0bc5345d768744e22424aa201c156b3cfa46f
Ian Beacall <ian@shogenapps.com>
2023-12-09 18:00:58 +0900

    Date Fixer: Fix some issues

--- src/features/date_fixer/date_fixer.js
+++ src/features/date_fixer/date_fixer.js
@@ -21,6 +21,8 @@ const euDateFormats = [
   "d-M-yyyy",
   "dd MMM yyyy",
   "d MMM yyyy",
+  "dd MMMM yyyy",
+  "d MMMM yyyy",
   "dd.MM.yyyy",
   "d.MM.yyyy",
   "dd.MM.yyyy",
@@ -46,66 +48,73 @@ const usDateFormats = [
   "M/dd/yyyy",
   "MM/d/yyyy",
   "M/d/yyyy",
+  "MMMM d yyyy",
+  "MMMM dd yyyy",
 ];
 
-const isoDateFormats = ["yyyy-MM-dd", "yyyy-M-d"];
+const isoDateFormats = [
+  "yyyy-MM-dd",
+  "yyyy-M-d",
+  "yyyy-MM-d",
+  "yyyy-M-dd",
+  "yyyy/MM/dd",
+  "yyyy/M/d",
+  "yyyy/MM/d",
+  "yyyy/M/dd",
+  "yyyy.MM.dd",
+  "yyyy.M.d",
+  "yyyy.MM.d",
+  "yyyy.M.dd",
+  "yyyy MM dd",
+  "yyyy M d",
+  "yyyy MM d",
+  "yyyy M dd",
+  "yyyy MMMM dd",
+  "yyyy MMM dd",
+  "yyyy MMMM d",
+  "yyyy MMM d",
+];
 
-// Function to handle and format the date
-/*
-function handleDateInput(dateString, inputElement) {
-  // Try parsing the date with both European and American formats
-  const euParsed = tryParseDate(dateString, euDateFormats);
-  const usParsed = tryParseDate(dateString, usDateFormats);
+function displayClarificationModal(dateString, ambiguousMonth, inputElement, possibleMonths) {
+  let modalContent = '<div id="dateClarificationModal"><p>Please clarify the month:</p>';
 
-  // Check if both interpretations are valid (ambiguous case)
-  if (isValid(euParsed) && isValid(usParsed)) {
-    const formattedEu = format(euParsed, "d MMM yyyy"); // Format European date
-    const formattedUs = format(usParsed, "d MMM yyyy"); // Format American date
+  // Create buttons for each possible month
+  possibleMonths.forEach((month) => {
+    modalContent += `<button class="monthOption">${month}</button>`;
+  });
 
-    const dateClarification = $(`
-      <div id="dateClarificationModal">
-        <p>Please clarify the date:</p>
-        <button id="optionEu">${formattedEu}</button>
-        <button id="optionUs">${formattedUs}</button>
-          <x>&#215;</x>       
-        </div>
-    `);
+  modalContent += '<x id="modalCancel">x</x></div>';
 
-    // Prompt the user to select the correct interpretation
+  // Append the modal to the document and set up event listeners
+  inputElement.after(modalContent);
+
+  // Event listener for each month option
+  $(".monthOption").on("click", function (e) {
+    e.preventDefault();
+    const selectedMonth = $(this).text();
+    const correctedDateString = dateString.replace(ambiguousMonth, selectedMonth);
+    inputElement.val(correctedDateString);
+    inputElement.trigger("change");
     $("#dateClarificationModal").remove();
-    inputElement.after(dateClarification);
-    $("#dateClarificationModal x").on("click", function () {
-      $("#dateClarificationModal").remove();
-    });
+  });
 
-    $("#dateClarificationModal").show();
+  // Event listener for the cancel button
+  $("#modalCancel").on("click", function (e) {
+    e.preventDefault();
+    $("#dateClarificationModal").remove();
+  });
 
-    $("#optionEu").on("click", function (e) {
-      e.preventDefault();
-      inputElement.val(formattedEu);
-      $("#dateClarificationModal").remove();
-    });
-    $("#optionUs").on("click", function (e) {
-      e.preventDefault();
-      inputElement.val(formattedUs);
-      $("#dateClarificationModal").remove();
-    });
-    return "Ambiguous date";
-  } else if (isValid(euParsed)) {
-    // Format and return the European interpretation
-    return format(euParsed, "dd MMM yyyy");
-  } else if (isValid(usParsed)) {
-    // Format and return the American interpretation
-    return format(usParsed, "dd MMM yyyy");
-  } else {
-    // Invalid date
-    //return "Invalid date";
-    displayWarning(inputElement, "Invalid date format.");
-  }
+  $("#dateClarificationModal").show();
 }
-*/
 
 function handleDateInput(dateString, inputElement) {
+  // Detect if the date string has an ambiguous month abbreviation
+  const ambiguousMonthMatch = dateString.match(/\b(Ju|J|M|A)\b/);
+  if (ambiguousMonthMatch) {
+    const possibleMonths = getAmbiguousMonths(ambiguousMonthMatch[0]);
+    return displayClarificationModal(dateString, ambiguousMonthMatch[0], inputElement, possibleMonths);
+  }
+
   // Try parsing the date with both European and American formats
   const euParsed = tryParseDate(dateString, euDateFormats);
   const usParsed = tryParseDate(dateString, usDateFormats);
@@ -114,6 +123,7 @@ function handleDateInput(dateString, inputElement) {
   if (isValid(euParsed) && isValid(usParsed)) {
     const formattedEu = format(euParsed, "d MMM yyyy"); // Format European date
     const formattedUs = format(usParsed, "d MMM yyyy"); // Format American date
+
     const dateClarification = $(`
       <div id="dateClarificationModal">
         <p>Please clarify the date:</p>
@@ -157,12 +167,15 @@ function handleDateInput(dateString, inputElement) {
 // Function to display a warning message
 function displayWarning(inputElement, message) {
   const warningElement = $(`<div id="dateWarning">${message}</div>`);
+  warningElement.on("click", function () {
+    warningElement.off().remove();
+  });
   $("#dateWarning").remove(); // Remove any existing warnings
   inputElement.after(warningElement);
 
   setTimeout(() => {
     warningElement.remove();
-  }, 5000); // Removes the warning after 5 seconds
+  }, 7000); // Removes the warning after 7 seconds
 }
 
 function sanitizeInput(input) {
@@ -302,36 +315,126 @@ const nonEnglishMonthNames = {
 
 const monthShortNames = monthNames.map((month) => month.substr(0, 3));
 
+// Function to check for ambiguous month abbreviations
+function getAmbiguousMonths(input) {
+  console.log("Checking for ambiguous months in input:", input);
+
+  const abbreviations = {
+    J: ["Jan", "Jun", "Jul"],
+    Ju: ["Jun", "Jul"],
+    M: ["Mar", "May"],
+    A: ["Apr", "Aug"],
+    // Add other ambiguous cases here
+  };
+
+  for (const key in abbreviations) {
+    const regex = new RegExp(`\\b${key}\\b`);
+    if (input.match(regex)) {
+      console.log(`Ambiguous abbreviation '${key}' found, possible months:`, abbreviations[key]);
+      return abbreviations[key];
+    }
+  }
+
+  console.log("No ambiguous abbreviations found in input.");
+  return null;
+}
+
+function parsedDate(input) {
+  // Try parsing the date with both European and American formats
+  const euParsed = tryParseDate(input, euDateFormats);
+  const usParsed = tryParseDate(input, usDateFormats);
+  const isoParsed = tryParseDate(input, isoDateFormats);
+  if (!isValid(euParsed) && !isValid(usParsed) && !isValid(isoParsed)) {
+    return null;
+  }
+  let validDate = false;
+  if (isValid(usParsed)) {
+    validDate = format(usParsed, "dd MMM yyyy");
+  } else if (isValid(euParsed)) {
+    validDate = format(euParsed, "dd MMM yyyy");
+  } else if (isValid(isoParsed)) {
+    validDate = format(isoParsed, "yyyy-MM-dd");
+  }
+  return { eu: euParsed, us: usParsed, iso: isoParsed, validDate: validDate };
+}
+
+const monthTypoPattern = /^(\d+)?\s*(\p{L}+)\s*(\d{1,2})?,?\s*(\d+)$/u;
+
+function fixMonthTypos(input) {
+  console.log("Original Input:", input);
+
+  if (monthTypoPattern.test(input)) {
+    const [, day, month, year] = monthTypoPattern.exec(input);
+    console.log(`Detected - Day: ${day}, Month: ${month}, Year: ${year}`);
+
+    // Replace non-English month names with English ones
+    let correctedMonth = nonEnglishMonthNames[month.toLowerCase()] || month;
+    console.log("After non-English month replacement:", correctedMonth);
+
+    // Use Fuse.js to correct typos in English month names
+    const fuse = new Fuse(monthNames, { includeScore: true, threshold: 0.4 });
+    const results = fuse.search(correctedMonth);
+    console.log("Fuse.js results:", results);
+
+    if (results.length > 0 && results[0].score < 0.4) {
+      correctedMonth = results[0].item;
+      console.log("After typo correction:", correctedMonth);
+    }
+
+    input = input.replace(month, correctedMonth);
+    console.log("Final Input:", input);
+  } else {
+    console.log("No month typo pattern match.");
+  }
+
+  return input;
+}
+
 function fixDates() {
   function parseDate(input, inputElement) {
+    console.log("Original input:", input);
+
     $("#dateWarning").remove(); // Remove any existing warnings
     $("#dateClarificationModal").remove(); // Remove any existing date clarification modal
 
     input = sanitizeInput(input);
+    console.log("After sanitization:", input);
 
-    // Try parsing the date with both European and American formats
-    const euParsed = tryParseDate(input, euDateFormats);
-    const usParsed = tryParseDate(input, usDateFormats);
-    const isoParsed = tryParseDate(input, isoDateFormats);
+    // Check for ambiguous month abbreviations
+    const ambiguousMonthMatch = input.match(/\b(Ju|J|M|A)\b/);
+    if (ambiguousMonthMatch) {
+      console.log("Found ambiguous month abbreviation:", ambiguousMonthMatch[0]);
+
+      const possibleMonths = getAmbiguousMonths(ambiguousMonthMatch[0]);
+      if (possibleMonths && possibleMonths.length > 1) {
+        displayClarificationModal(input, ambiguousMonthMatch[0], inputElement, possibleMonths);
+        return;
+      }
+    }
+
+    input = fixMonthTypos(input);
+    console.log("After fixing typos:", input);
+
+    // Use parsedDate function to get parsed dates
+    const parsed = parsedDate(input);
+    console.log("Parsed date result:", parsed);
 
     // Check if the date is ambiguous (both EU and US formats are valid)
-    if (isValid(euParsed) && isValid(usParsed)) {
-      if (window.dateFixerOptions["convertDD-MM-YYYY"] == "askMe") {
-        // Trigger the clarification modal for ambiguous dates
-        return handleDateInput(input, inputElement);
-      } else if (window.dateFixerOptions["convertDD-MM-YYYY"] == "always") {
-        // If the option is set to always, choose a format (here, we choose the EU format)
-        return format(euParsed, "dd MMM yyyy");
-      }
-      // If the option is set to never, no action is taken (you can add logic here if needed)
-    } else {
-      // If the date is not ambiguous, check individual formats
-      if (isValid(euParsed)) {
-        return format(euParsed, "dd MMM yyyy");
-      } else if (isValid(usParsed)) {
-        return format(usParsed, "dd MMM yyyy");
-      } else if (isValid(isoParsed)) {
-        return format(isoParsed, "yyyy-MM-dd");
+    if (parsed) {
+      if (isValid(parsed.eu) && isValid(parsed.us)) {
+        console.log("Ambiguous date detected");
+
+        if (window.dateFixerOptions["convertDD-MM-YYYY"] == "askMe") {
+          // Trigger the clarification modal for ambiguous dates
+          return handleDateInput(input, inputElement);
+        } else if (window.dateFixerOptions["convertDD-MM-YYYY"] == "always") {
+          // If the option is set to always, choose a format (here, we choose the EU format)
+          return format(parsed.eu, "dd MMM yyyy");
+        }
+        // If the option is set to never, no action is taken (you can add logic here if needed)
+      } else {
+        // If the date is not ambiguous, return the valid format
+        return parsed.validDate;
       }
     }
 
@@ -352,6 +455,8 @@ function fixDates() {
     ];
     for (const pattern of patterns) {
       if (pattern.test(input)) {
+        console.log("Pattern matched:", pattern);
+
         isValidDate = true; // Valid date pattern found
         const [, day, month, year] = pattern.exec(input);
         return `${String(day).padStart(2, "0")} ${monthShortNames[parseInt(month, 10) - 1]} ${year}`;
@@ -359,7 +464,7 @@ function fixDates() {
     }
 
     // Define a pattern for "YYYY MMM DD"
-    const yearFirstPattern = /^(\d{4}) ([a-zA-Z]+) (\d{1,2})$/;
+    const yearFirstPattern = /^(\d{4}) (\p{L}+) (\d{1,2})$/;
     if (yearFirstPattern.test(input)) {
       const [, year, month, day] = yearFirstPattern.exec(input);
 
@@ -374,8 +479,11 @@ function fixDates() {
     }
 
     // Fix typos in month names
-    const monthTypoPattern = /^(\d{1,2})?\s*([a-zA-Z]+)\s*(\d{1,2})?,?\s*(\d{4})$/;
+    const monthTypoPattern = /^(\d{1,2})?\s*(\p{L}+)\s*(\d{1,2})?,?\s*(\d{4})$/u;
+
     if (monthTypoPattern.test(input)) {
+      // input = fixMonthTypos(input);
+
       const [, day, month, year] = monthTypoPattern.exec(input);
 
       // Convert non-English month names to English
@@ -426,15 +534,23 @@ function fixDates() {
       /^(\d{4})$/, // YYYY
     ];
 
+    let message = "Invalid date format";
     const AABBYYYY = /^(\d{2})-(\d{2})-(\d{4})$/;
     for (const pattern of validPatterns) {
       if (pattern.test(input)) {
-        isValidDate = true; // Valid date pattern found
-        const [, day, month, year] = pattern.exec(input);
-        return `${String(day).padStart(2, "0")} ${monthShortNames[parseInt(month, 10) - 1]} ${year}`;
-      } else {
-        return input;
+        console.log("Pattern matched:", pattern);
+        message = "Invalid date";
+        if (parsedDate(input)) {
+          console.log("Parsed date:", parsedDate(input));
+          isValidDate = true; // Valid date pattern found
+          return parsedDate(input).validDate;
+        }
       }
+    }
+
+    if (!isValidDate) {
+      displayWarning(inputElement, message);
+      return "";
     }
 
     let firstBitIsDay = false;
@@ -460,8 +576,6 @@ function fixDates() {
       displayWarning(inputElement, "Invalid date format.");
       return "";
     }
-
-    //    return input;
   }
 
   $("#mBirthDate,#mDeathDate,#mMarriageDate,#photo_date,#mStartDate,#mEndDate").on("change", function () {
