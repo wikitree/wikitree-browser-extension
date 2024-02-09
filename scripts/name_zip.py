fd2f44f08aba42eedd65e60b63f9e933e77423d0
Flominatortm <flominator@gmx.net>
2024-02-09 20:14:35 +0100

    separate build stop for both renamed

--- scripts/name_zip.py
+++ scripts/name_zip.py
@@ -1,22 +1,16 @@
 import json
 import os
 
-version = ""
+def rename_dist(browser, version):
+    source = "dist-"+browser+".zip"
+    destination = "WBE_Preview_" + version + "_" + browser[0:1].upper() + browser[1:] +".zip"
+    if os.path.isfile(source):
+        if os.path.isfile(destination):
+            os.remove(destination)
+        os.rename(source, destination)
+
 with open('src/manifest/manifest-chrome.json') as f:
-    d = json.load(f)
-    version = d["version"]
-
-firefox = "WBE_Preview_" + version + "_Firefox.zip"
-chrome = "WBE_Preview_" + version + "_Chrome.zip"
-
-if os.path.isfile(firefox):
-    os.remove(firefox)
-
-if os.path.isfile(chrome):
-    os.remove(chrome)
-
-if os.path.isfile("dist-firefox.zip"):
-    os.rename("dist-firefox.zip", firefox)
-
-if os.path.isfile("dist-chrome.zip"):
-    os.rename("dist-chrome.zip", chrome)
+    manifest = json.load(f)
+    rename_dist("chrome", manifest["version"])
+    rename_dist("firefox", manifest["version"])
+    
