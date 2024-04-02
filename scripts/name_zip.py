b7562f74465c0d43d743ee41056b8f3363406172
Flominatortm <flominator@gmx.net>
2024-04-02 20:56:17 +0200

    automatic file names for local preview versions by separate job

--- scripts/name_zip.py
+++ scripts/name_zip.py
@@ -1,3 +1,4 @@
+# gives meaningful names to files for both browsers and adds an additional letter part (used for in-between preview versions)
 import json
 import os
 
@@ -9,8 +10,26 @@ def rename_dist(browser, version):
             os.remove(destination)
         os.rename(source, destination)
 
+version_save_file = "lastVersion.txt"
+
 with open('src/manifest/manifest-chrome.json') as f:
+    version_letter = "a"
     manifest = json.load(f)
-    rename_dist("chrome", manifest["version"])
-    rename_dist("firefox", manifest["version"])
+    version_number = manifest["version"]
+    
+    if os.path.isfile (version_save_file ):
+        print("version file found")
+        with open(version_save_file , "r") as v:
+            version_parts = v.readlines();
+            print(f"previous version: {version_parts}")
+            if version_parts[0] == version_number+ "\n":
+                print("same basic version")
+                version_letter =  chr(ord(version_parts[1]) + 1)
+            else:
+                version_letter = "a"
+    version_name = version_number+version_letter
+    rename_dist("chrome", version_name)
+    rename_dist("firefox", version_name)
+    with open(version_save_file , "w") as v:
+        v.write(f"{version_number}\n{version_letter}")
     
