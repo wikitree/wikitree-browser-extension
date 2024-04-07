6215b4a87b13016ab0202d1f70a4e03c78a26af1
Ian Beacall <ian@shogenapps.com>
2024-04-07 12:39:07 +0900

    Update change_family_lists.js

--- src/features/change_family_lists/change_family_lists.js
+++ src/features/change_family_lists/change_family_lists.js
@@ -1854,14 +1854,19 @@ function amaTimer() {
         );
 
         const marriageId = "marriage_" + aSpouse[0];
-        marriageDiv.get(0).innerHTML = marriageDiv
-          .get(0)
-          .innerHTML.replace(
+        const marriageDetails = marriageDiv.find(".marriageDetails");
+        // Perform the replacement
+        marriageDetails.html(function (index, html) {
+          return html.replace(
             "married",
-            '<span title="Right click to edit marriage" class="clickable" id="' + marriageId + '">married</span>'
+            '<a href="#" title="Right click to edit marriage" class="clickable" id="' + marriageId + '">married</a>'
           );
+        });
 
-        $("#" + marriageId).on("contextmenu", function (e) {
+        // Now wrap the entire content in a single inline-block element due to the CSS grid
+        marriageDetails.contents().wrapAll('<div style="inline-block"></div>');
+
+        $("#" + marriageId).on("click", function (e) {
           e.preventDefault();
           editMarriage(window.people[0].Id, aSpouse[0]);
         });
