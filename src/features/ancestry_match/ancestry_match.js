73cb967d73d6f4d65e4813eddcf4952534f7062c
Ian Beacall <ian@shogenapps.com>
2024-09-28 18:05:15 +0900

    Update ancestry_match.js
    Working on it.

--- src/features/ancestry_match/ancestry_match.js
+++ src/features/ancestry_match/ancestry_match.js
@@ -1,36 +1,62 @@
-//import $ from "jquery";
+import $ from "jquery";
 import { shouldInitializeFeature } from "../../core/options/options_storage";
-import { WikiTreeAPI } from "../../core/API/WikiTreeAPI";
 
 shouldInitializeFeature("ancestryMatch").then((result) => {
-  if (result) {
+  if (result && window.location.href.match(/ancestry.*\/family-tree\/tree/)) {
     console.log("Ancestry Match feature is enabled");
     init();
   }
 });
 
-async function init() {
-  const personId = "Beacall-6";
-  const people = await getThePeople(personId);
-  console.log(people);
+let ancestryData;
+
+function init() {
+  const topRightButtons = $(".topRightButtons");
+  const wtidBox = $("<input>").prop("id", "wtid-box").prop("type", "text").prop("placeholder", "Enter WikiTree ID");
+  const wtidButton = $("<button>").prop("id", "wtid-button").text("Get Ancestry Match");
+  const wtidContainer = $("<div>").append(wtidBox).append(wtidButton);
+  setTimeout(() => {
+    topRightButtons.append(wtidContainer);
+  }, 5000);
+
+  wtidButton.on("click", function (e) {
+    e.preventDefault();
+    const wtid = (wtidBox.val() + " ").trim();
+    getPeople(wtid);
+
+    if (!ancestryData) {
+      getAncestryData();
+    }
+  });
 }
 
-async function getThePeople(id) {
-  //  WikiTreeAPI.getPeople(appId, nextIDsToLoad, ["Id", "Name", "LastNameAtBirth"], { ancestors: 5, minGeneration:3 }).then(
-  const fields = [
-    "Id",
-    "Name",
-    "FirstName",
-    "MiddleName",
-    "LastNameAtBirth",
-    "BirthDate",
-    "BirthLocation",
-    "DeathDate",
-    "DeathLocation",
-  ];
-  const options = { ancestors: 5, descendants: 5 };
-  const appId = "WBE_ancestryMatch";
-  const ids = [id];
-  const people = await WikiTreeAPI.getPeople(appId, ids, fields, options);
-  return people;
+async function getAncestryData() {
+  const pageUrl = window.location.href;
+  // https://www.ancestry.com/family-tree/tree/177688262/family?cfpid=222309221861&fpid=222309221861
+  const urlParts = pageUrl.split("/");
+  const treeId = urlParts[6];
+  const cfpid = urlParts[8].split("&")[0].split("=")[1];
+  const url = `https://www.ancestry.com/family-tree/tree/${treeId}/family?cfpid=${cfpid}&fpid=${cfpid}`;
+  fetch(url)
+    .then((response) => response.text())
+    .then((data) => {
+      // Handle the data
+      console.log("Received people data:", data);
+    })
+    .catch((error) => {
+      console.error("Error fetching people data:", error);
+    });
+}
+
+async function getPeople(personId) {
+  // Fetch the result from your PHP server
+  fetch(`https://apps.wikitree.com/apps/beacall6/api/api.php?id=${personId}&ancestors=5&descendants=5`)
+    .then((response) => response.json())
+    .then((data) => {
+      // Handle the data
+      console.log("Received people data:", data);
+    })
+    .catch((error) => {
+      console.error("Error fetching people data:", error);
+    });
 }
