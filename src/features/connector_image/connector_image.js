88161e846205f840e456d2aab2145d129ecbc9e3
Ian Beacall <ian@shogenapps.com>
2025-02-08 14:58:32 +0900

    Fix jigsawMan

--- src/features/connector_image/connector_image.js
+++ src/features/connector_image/connector_image.js
@@ -5,15 +5,14 @@ import { profilePerson } from "../sort_theme_people/sort_theme_people.js";
 shouldInitializeFeature("connectorImage").then((result) => {
   if (result) {
     const name = profilePerson.FullName;
-    const hasCFProfiles = $("div.sixteen.columns a[href*='Special:Connection']").length > 8;
-    const hasJigsawMan = $("h1 span.icon--unconnected").length > 0;
+    const hasCFProfiles = $("section.connections").length > 0;
+    const hasJigsawMan = $("div.page--title span.icon--unconnected").length > 0;
     const id = profilePerson.Name;
     if (!hasCFProfiles && !hasJigsawMan) {
       const jigsawMan = $(
         `<a id="jigsawMan" href="https://apps.wikitree.com/apps/nelson3486/connections/index.html?ID=${id}&steps=3" target="_blank" title="${name} is not connected to our global tree; click to see missing links"><span class="icon--unconnected"></span></a>`
       );
-      $(`h1 small.lifespan`).after(jigsawMan);
-      jigsawMan.css({ "margin-left": "1em" });
+      $(`div.page--title span.privacy`).before(jigsawMan);
     }
   }
 });
--- src/features/sort_theme_people/sort_theme_people.js
+++ src/features/sort_theme_people/sort_theme_people.js
@@ -168,7 +168,10 @@ export function getProfilePersonInfo() {
     return null;
   }
   person.Name = pageData.mnamedb;
-  person.FullName = $("h1 span[itemprop='name']").text().trim();
+  // Clone h1, remove all children and trim the text.
+  const $h1 = $("h1").clone();
+  $h1.children().remove();
+  person.FullName = $h1.text().trim();
   person.Id = pageData.mid;
   person.LastNameAtBirth = pageData.mlastnameatbirth;
   person.FirstName = pageData.mfirstname;
