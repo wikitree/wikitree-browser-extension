cee1c4c1d67d2673acef4eaa3500abb58b09e268
Ian Beacall <ian@shogenapps.com>
2025-02-18 01:50:34 +0900

    Enumerate image detail:  Add Copy List button

--- src/features/enumerate_image_detail/enumerate_image_detail.js
+++ src/features/enumerate_image_detail/enumerate_image_detail.js
@@ -1,9 +1,26 @@
 import $ from "jquery";
+import { showCopyMessage } from "../access_keys/access_keys";
 import { shouldInitializeFeature } from "../../core/options/options_storage";
 
-shouldInitializeFeature("enumerateImageDetail").then((result) => {
+shouldInitializeFeature("enumerateImageDetail").then(async (result) => {
   if (result) {
-    import("./enumerate_image_detail.css");
+    await import("./enumerate_image_detail.css");
+
+    $(document).on("click", ".copyButton", function () {
+      let text = "";
+      $(this)
+        .closest("div")
+        .find("ol.enumerated li")
+        .each(function () {
+          const link = $(this).find(`a[itemprop="url"]`);
+          const personName = link.text().trim();
+          const personID = link.attr("href").split("/wiki/")[1].replace(/_/g, " ");
+          text += `# [[${personID}|${personName}]]\n`;
+        });
+      navigator.clipboard.writeText(text);
+      showCopyMessage("list");
+    });
+
     $("ul.STYLED")
       .has("li.BULLET60 span[itemprop='about']")
       .each(function () {
@@ -11,6 +28,8 @@ shouldInitializeFeature("enumerateImageDetail").then((result) => {
 
         // Convert <ul> to <ol>
         let $ol = $("<ol>").html($ul.html()).attr("class", $ul.attr("class")).addClass("enumerated");
+        const copyButton = $("<button>").addClass("small button copyButton").text("Copy list");
+        $(this).parent().find(`a[data-bs-title="Edit Identified Profiles"]`).after(copyButton);
 
         // Get all <li> elements and sort them based on the text inside the <span itemprop="about">
         let $sortedLis = $ol.children("li.BULLET60").sort(function (a, b) {
