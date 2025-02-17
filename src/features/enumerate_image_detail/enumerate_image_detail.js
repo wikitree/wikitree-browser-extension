56df1038c9f9776b0dc32b8fa47fffe3db784c07
Ian Beacall <ian@shogenapps.com>
2025-02-18 02:09:32 +0900

    Enumerate Image Detail: Add copy button

--- src/features/enumerate_image_detail/enumerate_image_detail.js
+++ src/features/enumerate_image_detail/enumerate_image_detail.js
@@ -1,7 +1,24 @@
 import { shouldInitializeFeature } from "../../core/options/options_storage";
+import $ from "jquery";
+import { showCopyMessage } from "../access_keys/access_keys";
 
 shouldInitializeFeature("enumerateImageDetail").then((result) => {
   if (result) {
+    $(document).on("click", ".copyButton", function () {
+      let text = "";
+      $(this)
+        .closest("div")
+        .find("ol.STYLED li")
+        .each(function () {
+          const link = $(this).find(`a[itemprop="url"]`);
+          const personName = link.text().trim();
+          const personID = link.attr("href").split("/wiki/")[1].replace(/_/g, " ");
+          text += `# [[${personID}|${personName}]]\n`;
+        });
+      navigator.clipboard.writeText(text);
+      showCopyMessage("list");
+    });
+
     $("ul.STYLED")
       .has("li.BULLET60 span[itemprop='about']")
       .each(function () {
@@ -9,6 +26,8 @@ shouldInitializeFeature("enumerateImageDetail").then((result) => {
 
         // Convert <ul> to <ol>
         let $ol = $("<ol>").html($ul.html()).attr("class", $ul.attr("class"));
+        const copyButton = $("<button>").addClass("small button copyButton").text("Copy list").css("float", "right");
+        $(this).parent().find(`p:Contains("In this image")`).append(copyButton);
 
         // Get all <li> elements and sort them based on the text inside the <span itemprop="about">
         let $sortedLis = $ol.children("li.BULLET60").sort(function (a, b) {
