7426d6c9b0e42789b405e96c82a8310fcca11ed7
Ian Beacall <ian@shogenapps.com>
2025-02-19 22:12:38 +0900

    Fix DNA Tests table

--- src/features/dna_table/dna_table.js
+++ src/features/dna_table/dna_table.js
@@ -8,18 +8,30 @@ import { shouldInitializeFeature } from "../../core/options/options_storage";
 function getBirthplaces() {
   const ids = [];
   const idSpans = $("span.SMALL");
+
   idSpans.each(function () {
     let anId = $(this).text();
     if (!ids.includes(anId)) {
       ids.push(anId);
     }
   });
+
   setTimeout(function () {
     const theTable = $("table.table");
     let rows = theTable.find("tr");
-    const tHead = $("<thead></thead>");
-    tHead.prependTo(theTable);
-    rows.eq(0).appendTo(tHead).find("th").eq(0).after($("<th id='birthplace' data-order=''>Birthplace</th>"));
+
+    // Create table header for Birthplace column if it doesn't exist
+    if ($("#birthplace").length === 0) {
+      const tHead = $("<thead></thead>");
+      tHead.prependTo(theTable);
+      rows
+        .eq(0)
+        .appendTo(tHead)
+        .find("th")
+        .eq(0)
+        .after($("<th id='birthplace' class='table-secondary' data-order=''>Birthplace</th>"));
+    }
+
     rows = theTable.find("tbody tr");
     rows.each(function () {
       let id = $(this).find("span.SMALL").text();
@@ -29,61 +41,48 @@ function getBirthplaces() {
         .after($("<td class='birthplace' data-id='" + id + "'></td>"));
       $(this).attr("data-birth-location", "").attr("data-birth-location-reversed", "");
     });
+
+    // Fetch data in chunks of 100
     while (ids.length) {
       let chunk = ids.splice(0, 100).join(",");
       getPeople(chunk, 0, 0, 0, 0, 0, "Id,Name,BirthLocation", "WBE_dna_table").then((data) => {
-        let theKeys = Object.keys(data[0].people);
-        theKeys.forEach(function (aKey) {
-          let person = data[0].people[aKey];
-          let reversedBirthPlace = "";
-          if (person.BirthLocation) {
-            let bpSplit = person.BirthLocation.split(", ");
-            reversedBirthPlace = bpSplit.reverse().join(", ");
-          }
-          $(this).find(".birthplace").text(reversedBirthPlace);
-          $(`td[data-id='${person.Name}']`)
-            .text(person.BirthLocation)
-            .closest("tr")
-            .attr("data-birth-location", person.BirthLocation)
-            .attr("data-birth-location-reversed", reversedBirthPlace);
-        });
+        if (data[0] && data[0].people) {
+          let theKeys = Object.keys(data[0].people);
+          theKeys.forEach(function (aKey) {
+            let person = data[0].people[aKey];
+            let birthplace = person.BirthLocation || "";
+            let reversedBirthPlace = birthplace.split(", ").reverse().join(", ");
+
+            let cell = $(`td[data-id='${person.Name}']`);
+            cell.text(birthplace);
+            let row = cell.closest("tr");
+
+            row.attr("data-birth-location", birthplace);
+            row.attr("data-birth-location-reversed", reversedBirthPlace);
+          });
+        }
       });
     }
-    $("#birthplace").on("click", function () {
-      rows = $("table.wt.names tbody tr");
-      let theOrder = $("#birthplace").data("order");
-      let theSorter;
-      if (theOrder == "smallFirst") {
-        $("#birthplace").data("order", "bigFirst");
-        theSorter = "birth-location-reversed";
-        rows.each(function () {
-          $(this).find(".birthplace").text($(this).data("birth-location-reversed"));
-        });
-      } else {
-        $("#birthplace").data("order", "smallFirst");
-        theSorter = "birth-location";
-        rows.each(function () {
-          $(this).find(".birthplace").text($(this).data("birth-location"));
-        });
-      }
-      for (let i = 0; i < 2; i++) {
+
+    // Sorting functionality for Birthplace column
+    $("#birthplace")
+      .off("click")
+      .on("click", function () {
+        let rows = $("table.table tbody tr").detach(); // Detach rows for efficient sorting
+        let theOrder = $(this).attr("data-order") || "asc";
+        let theSorter = theOrder === "asc" ? "data-birth-location" : "data-birth-location-reversed";
+
+        // Toggle sorting order
+        $(this).attr("data-order", theOrder === "asc" ? "desc" : "asc");
+
         rows.sort(function (a, b) {
-          if ($(a).data(theSorter) == undefined) {
-            $(a).data(theSorter, " ");
-          }
-          if ($(b).data(theSorter) == undefined) {
-            $(b).data(theSorter, " ");
-          }
-          return $(a).data(theSorter).localeCompare($(b).data(theSorter));
+          let valA = $(a).attr(theSorter) || "";
+          let valB = $(b).attr(theSorter) || "";
+          return valA.localeCompare(valB);
         });
-        $("table.wt.names tbody").append(rows);
-        rows.each(function () {
-          if (["", " "].includes($(this).data("birth-location"))) {
-            theTable.find("tbody").append($(this));
-          }
-        });
-      }
-    });
+
+        $("table.table tbody").append(rows); // Re-append sorted rows
+      });
   }, 3000);
 }
 
@@ -94,10 +93,18 @@ shouldInitializeFeature("dnaTable").then((result) => {
     $("<button class='button small tight' id='showBirthplacesButton'>Add birthplaces</button>").prependTo(
       $("a.density-on").parent()
     );
+
     $("#showBirthplacesButton").on("click", function (e) {
       e.preventDefault();
       getBirthplaces();
       $(this).prop("disabled", true);
+
+      // Fade out and remove the button after 3 seconds
+      setTimeout(function () {
+        $("#showBirthplacesButton").fadeOut(1000, function () {
+          $(this).remove();
+        });
+      }, 3000);
     });
   }
 });
@@ -117,6 +124,7 @@ export async function getPeople(keys, siblings, ancestors, descendants, nuclear,
         getSpouses: 1,
         appId: appId || "WBE",
       };
+
       // Remove all empty values
       Object.keys(data).forEach((key) => {
         if (data[key] === undefined || data[key] === null || data[key] === "") {
