8f353d1a4aeb9a6bd31caf1bc071cabab3fc2dd2
Ian Beacall <ian@shogenapps.com>
2025-03-11 05:17:57 +0900

    New: Show Search (show the search bar)

---
+++ src/features/show_search/show_search.js
@@ -0,0 +1,26 @@
+/*
+Created By: Ian Beacall (Beacall-6)
+*/
+
+import { shouldInitializeFeature } from "../../core/options/options_storage";
+import { isG2G } from "../../core/pageType";
+import $ from "jquery";
+
+function showSearch() {
+  const searchBar = $("#searchBar");
+  searchBar.addClass("showSearch show");
+  $("#wpFirstHdr").attr("placeholder", "First Name");
+  $("#wpLastHdr").attr("placeholder", "Last Name");
+  $("#watchlistSuggestionKeys").attr("placeholder", "Search Watchlist");
+}
+
+shouldInitializeFeature("showSearch").then((result) => {
+  if (result && !isG2G) {
+    import("./show_search.css").then(() => {
+      showSearch();
+      $("nav button.btn-search").one("click", () => {
+        $("#searchBar").removeClass("showSearch show");
+      });
+    });
+  }
+});
