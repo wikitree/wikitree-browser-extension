2bafe1e087e1dfe6ee1b07cd158fdb75a352ae96
Ian Beacall <ian@shogenapps.com>
2025-03-11 06:06:27 +0900

    Fix Category Display

--- src/features/categoryDisplay/categoryDisplay.js
+++ src/features/categoryDisplay/categoryDisplay.js
@@ -7,26 +7,29 @@ import { shouldInitializeFeature, getFeatureOptions } from "../../core/options/o
 
 shouldInitializeFeature("categoryDisplay").then((result) => {
   if (result) {
-    moveCategories();
+    import("./category_display.css").then(() => {
+      moveCategories();
+    });
   }
 });
 
 async function moveCategories() {
   const options = await getFeatureOptions("categoryDisplay");
   // Determine the display type
+  const $categories = $("#Categories");
   switch (options.displayType) {
     case "default":
       break;
     case "list":
-      $("#categories").find('span[class="SMALL"]').remove();
-      $("#categories").replaceWith(function () {
-        var listCats = $("#categories")
+      $categories.find('span[class="SMALL"]').remove();
+      $categories.replaceWith(function () {
+        var listCats = $categories
           .html()
           .replace(/\|/g, "")
           .replace(/&nbsp;/g, "");
-        return `<div id="categories"><ol class="star">${listCats}</ol></div>`;
+        return `<div id="Categories"><ol class="star">${listCats}</ol></div>`;
       });
-      $("#categories span").replaceWith(function () {
+      $categories.find("span").replaceWith(function () {
         //span needed for feature Category Management to find the categories within the div
         return `<li><span>${this.innerHTML}</span></li>`;
       });
@@ -35,32 +38,34 @@ async function moveCategories() {
   // Determine the border color
   switch (options.borderColor) {
     case "none":
-      $("#categories").css({
+      $categories.css({
         border: "none",
         padding: "5px",
         "margin-top": "10px",
       });
       break;
     case "gray":
-      $("#categories").attr("class", "box rounded row").css("margin-top", "10px");
+      $categories.attr("class", "box rounded row").css("margin-top", "10px");
       break;
     case "default":
-      $("#categories").attr("class", "box green rounded row").css("margin-top", "10px");
+      $categories.attr("class", "box green rounded row").css("margin-top", "10px");
       break;
     case "orange":
-      $("#categories").attr("class", "box orange rounded row").css("margin-top", "10px");
+      $categories.attr("class", "box orange rounded row").css("margin-top", "10px");
       break;
   }
   // Determine the category placement
+  let $biography = $("#Biography");
+  let $sidebar = $("#Profile-Data").closest("div");
   switch (options.categoryLocation) {
     case "sidebar":
-      $("#categories").addClass("row").find('span[class="SMALL"]').remove();
-      let $sidebar = $(".columns.six").first();
+      $categories.addClass("row").find('span[class="SMALL"]').remove();
+
       if ($sidebar.length > 0) {
         // space pages don't have a DNA section, so we just need to find the first section element
-        $sidebar.find("div > span.large").first().closest("div").before($("#categories"));
+        $sidebar.prepend($categories);
         // if it's in list form (with no border box)...
-        $("#categories:not(.box) > ol")
+        $("#Categories:not(.box) > ol")
           // ... reformat the heading match the rest of the sidebar ...
           .closest("div")
           .removeAttr("style")
@@ -77,14 +82,8 @@ async function moveCategories() {
       }
       break;
     case "top":
-      $("#categories").find('span[class="SMALL"]').remove();
-      let $target = $('div[style*="background-color:#eee"]:contains("page has been accessed")');
-      let $next = $target.next();
-      if ($next.is('div[style*="clear"]')) {
-        // sometimes this div is missing on free-space pages that are managed by the logged in user
-        $target = $next;
-      }
-      $target.after($("#categories"));
+      $categories.find('span[class="SMALL"]').remove();
+      $biography.before($categories);
       break;
     case "default":
       break;
