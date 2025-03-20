d6d25dd6f519c6f7bb3c5d6e9a56595a188cacd0
Ian Beacall <ian@shogenapps.com>
2025-03-20 19:06:05 +0900

    Toggle Photo Column: Switch off EE

--- src/features/hide_photo_column/hide_photo_column.js
+++ src/features/hide_photo_column/hide_photo_column.js
@@ -12,16 +12,45 @@ shouldInitializeFeature("hidePhotoColumn").then((result) => {
   }
 });
 
+const enhancedEditorOnLoadValue = $("#toggleMarkupColor").val();
+let enhancedEditorOnLoad;
+if (enhancedEditorOnLoadValue) {
+  enhancedEditorOnLoad = enhancedEditorOnLoadValue.toLowerCase() == "turn off enhanced editor" ? true : false;
+}
 const photoH2 = $("h2#Photo,h2#photo");
 const photoParent = photoH2.parent();
 const unicodePlus = "\u002B";
 const unicodeMinus = "\u2212";
 
+const buttonTitleHide = "Hide the photo column";
+const buttonTitleShow = "Show the photo column";
+const buttonTitleEESHide = "Turn off enhanced editor and hide the photo column";
+const buttonTitleEEShow = "Turn on enhanced editor and show the photo column";
+const minusURL = chrome.runtime.getURL("images/minus-toggler.svg");
+const plusURL = chrome.runtime.getURL("images/plus-toggler.svg");
+
+function setButtonTitle() {
+  if ($("body").hasClass("hiddenSidebar")) {
+    if (enhancedEditorOnLoad) {
+      $("#toggleTipsColumn").attr("data-tooltip", buttonTitleEEShow);
+    } else {
+      $("#toggleTipsColumn").attr("data-tooltip", buttonTitleShow);
+    }
+  } else {
+    if (enhancedEditorOnLoad) {
+      $("#toggleTipsColumn").attr("data-tooltip", buttonTitleEESHide);
+    } else {
+      $("#toggleTipsColumn").attr("data-tooltip", buttonTitleHide);
+    }
+  }
+}
+
 async function init() {
   if (photoH2.length) {
     photoH2.append(
-      `<button id="toggleTipsColumn" class="toggleTipsColumn small" title="Hide/show the photo column"><span class="minus">${unicodeMinus}</span><span class="plus">${unicodePlus}</span></button>`
+      `<a id="toggleTipsColumn" class="toggleTipsColumn small wbe-button" data-tooltip=""><span class="minus icon--toggler" style="background-image:url(${minusURL})"></span><span class="plus icon--toggler"  style="background-image:url(${plusURL})"></span></button>`
     );
+    setButtonTitle();
     $(document).on("click", "#toggleTipsColumn", function (e) {
       e.preventDefault();
       toggleTipsColumn();
@@ -36,6 +65,12 @@ async function init() {
 }
 
 function toggleTipsColumn() {
-  photoParent.children("*").not("#Photo,#photo").toggle();
-  $("body").toggleClass("hiddenSidebar");
+  if (enhancedEditorOnLoad) {
+    $("#toggleMarkupColor").trigger("click");
+  }
+  setTimeout(() => {
+    photoParent.children("*").not("#Photo,#photo").toggle();
+    $("body").toggleClass("hiddenSidebar");
+    setButtonTitle();
+  }, 100);
 }
