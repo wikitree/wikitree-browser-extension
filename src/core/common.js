a25e8440998f218d6a30e68091cc5c22dcf2eab3
Ian Beacall <ian@shogenapps.com>
2025-05-23 04:35:33 +0900

    WBE highlight -- in progress

--- src/core/common.js
+++ src/core/common.js
@@ -277,12 +277,17 @@ oncePerTab((rootWindow) => {
 if ($("#ebWBE").length == 0) {
   $('a img[alt="WikiTree: Where genealogists collaborate"]').parent("a")
     .after(`<span id="ebWBE"><a style="color: inherit !important; text-decoration: none;"
-        href="/wiki/Space:WikiTree_Browser_Extension">Enhanced by the WikiTree Browser Extension</a><button id="showWBEFeatures">Toggle WBE highlighting</button></span>`);
+        href="/wiki/Space:WikiTree_Browser_Extension">Enhanced by the WikiTree Browser Extension</a>
+        <a href="/wiki/Space:WikiTree_Browser_Extension" target="Help" id="showWBEFeatures" class="wbe-icon WBEHelpIcon enhancedBy" data-tooltip="Help" title="">
+            <img src="https://www.wikitree.com/images/icons/icon-question.svg" alt="Help" />
+          </a>
+          <button id="showWBEFeatures" title="Show WBE features">✨</button></span>`);
   $("header").on("click", "#showWBEFeatures", function (e) {
     e.preventDefault();
     $("body").toggleClass("wbe-highlight");
   });
 }
+// 🪄🖍️
 
 // Add wte class to body to let WikiTree BEE know not to add the same functions
 document.querySelector("body").classList.add("wte");
