f6b24a7395994e3e36c9e992c579743fccf0817c
Ian Beacall <ian@shogenapps.com>
2025-06-11 21:08:17 +0900

    Update text_expander_options.js

--- src/features/text_expander/text_expander_options.js
+++ src/features/text_expander/text_expander_options.js
@@ -5,7 +5,9 @@ registerFeature({
   name: "Text Expander",
   id: "textExpander",
   description:
-    "Automatically expands your own abbreviations as you type. Set your own abbreviations by clicking the arrow icon with the other WBE buttons.",
+    "Automatically expands your own abbreviations as you type. " +
+    "Set your own abbreviations by clicking the arrow icon with the other WBE buttons. " +
+    "(Not compatible with the Enhanced Editor.)",
   category: "Global",
   creators: [{ name: "Ian Beacall", wikitreeid: "Beacall-6" }],
   contributors: [],
