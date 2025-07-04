55bda12b0354514b9bad90a983eb259e643a0573
Ian Beacall <ian@shogenapps.com>
2025-07-04 09:49:36 +0900

    Bump version to 2.4.4.0 and remove old comments
    Updated the extension version to 2.4.4.0 in both Chrome and Firefox manifests. Removed outdated explanatory comments from activity_feed_filters.js for cleaner code.

--- src/features/activity_feed_filters/activity_feed_filters.js
+++ src/features/activity_feed_filters/activity_feed_filters.js
@@ -4,20 +4,6 @@ Created By: Ian Beacall (Beacall-6)
 import $ from "jquery";
 import { shouldInitializeFeature } from "../../core/options/options_storage";
 
-/* 
-Add buttons btn-secondary btn-sm: Append to #header .page--title; Only: Profiles, Space Pages
-Clicking will show only one or the other.
-
-A space one looks like this:
-
-<span class="feed-item"> 18:10: <a href="https://www.wikitree.com/wiki/Cairns-253" title="" target="_blank">Suzy Cairns</a> <a href="https://www.wikitree.com/index.php?title=Space:Greeters_Sign_In_Out_Page&amp;diff=225709081&amp;oldid=225704363" title="Space:Greeters Sign In Out Page" rel="nofollow" target="_blank">edited the Text</a> on <a href="/wiki/Space:Greeters_Sign_In_Out_Page" title="" target="_blank">Greeters Sign In Out Page</a>.<span class="btn btn-utility ms-2"><a href="https://www.wikitree.com/index.php?title=Special:Thanks&amp;action=thank&amp;rc_id=264416625" title="Special:Thanks" target="_blank">Thank Suzy for this</a></span></span>
-
-A profile one looks like this:
-
-<span class="feed-item"> 05:23: <a href="https://www.wikitree.com/wiki/Hobbs-1709" title="" target="_blank">Lesley (Hobbs) Scott</a> <a href="https://www.wikitree.com/index.php?title=Booth-1636&amp;diff=225612265&amp;oldid=189592037" title="Booth-1636" rel="nofollow" target="_blank"> edited the Birth Place </a> for <a href="https://www.wikitree.com/wiki/Booth-1636" title="" target="_blank">Kirby Booth (1900-1984)</a>. <span class="USERCOMMENT">(Minor corrections. )</span><span class="btn btn-utility ms-2"><a href="https://www.wikitree.com/index.php?title=Special:Thanks&amp;action=thank&amp;rc_id=264298672" title="Special:Thanks" target="_blank">Thank Lesley for this</a></span></span>
-
-*/
-
 function init() {
   /* 1 ─ Add the “Only: Profiles / Spaces” buttons */
   $("#header .page--title").append(`
