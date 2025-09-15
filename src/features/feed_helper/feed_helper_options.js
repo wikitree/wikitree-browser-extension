011b42425d230b9b7fb20659c5fe95fef40cf8ff
Ian Beacall <ian@shogenapps.com>
2025-09-16 03:12:20 +0900

    Initial Feed Helper additions

---
+++ src/features/feed_helper/feed_helper_options.js
@@ -0,0 +1,17 @@
+/*
+Created By: Ian Beacall (Beacall-6)
+*/
+
+import { registerFeature, OptionType } from "../../core/options/options_registry";
+import { isNetworkFeed } from "../../core/pageType";
+
+registerFeature({
+  name: "Feed Helper",
+  id: "feedHelper",
+  description: "",
+  category: "Other",
+  creators: [{ name: "Ian Beacall", wikitreeid: "Beacall-6" }],
+  contributors: [],
+  defaultValue: false,
+  pages: [ isNetworkFeed],
+});
