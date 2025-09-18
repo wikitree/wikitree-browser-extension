d5c58cb36417c1a17dd036a5a1a85396a49b109f
Ian Beacall <ian@shogenapps.com>
2025-09-19 02:59:45 +0900

    Squashed commit of the following:
    commit c3457dcb67ae0dca1f152c030e0f83b11bcabd86
    Author: Ian Beacall <ian@shogenapps.com>
    Date:   Fri Sep 19 02:59:19 2025 +0900
    
        Update feed_helper.js
    
    commit 46937e396f9b46001cfdb79ec1291ccc82dde3c5
    Author: Ian Beacall <ian@shogenapps.com>
    Date:   Fri Sep 19 02:00:57 2025 +0900
    
        Update feed_helper.js
    
    commit 624c291633c19d6ce3e23762cb2e025d6dd40e9c
    Author: Ian Beacall <ian@shogenapps.com>
    Date:   Fri Sep 19 02:00:50 2025 +0900
    
        Update feed_helper.js
    
    commit dda18a7c3f79edf596c91c69ca785c783dd1474d
    Author: Ian Beacall <ian@shogenapps.com>
    Date:   Fri Sep 19 00:38:05 2025 +0900
    
        Update feed_helper.js
    
    commit 1561070a01fdee1f3b0ec90163a25a690ae63c7a
    Author: Ian Beacall <ian@shogenapps.com>
    Date:   Thu Sep 18 23:58:34 2025 +0900
    
        Update feed_helper.js
    
    commit 7f1913590d0cd836b212cea0ce45f1bc3ef71f34
    Author: Ian Beacall <ian@shogenapps.com>
    Date:   Wed Sep 17 14:22:55 2025 +0900
    
        Update feed_helper.js
    
    commit 285a3b8f85c02958361a82e219478aca422b083c
    Author: Ian Beacall <ian@shogenapps.com>
    Date:   Wed Sep 17 14:22:43 2025 +0900
    
        Feed Helper: Many changes
    
    commit 1f0103ac9b69a9378c0fa23d3e214a0f1a6d232e
    Author: Ian Beacall <ian@shogenapps.com>
    Date:   Tue Sep 16 21:10:22 2025 +0900
    
        Update feed_helper_options.js
    
    commit 8d035fc9924f9f4a2e21d2f86fbb1d6f513aedcf
    Author: Ian Beacall <ian@shogenapps.com>
    Date:   Tue Sep 16 21:07:09 2025 +0900
    
        Add to more feeds and change buttons
    
    commit 2cc99436f2a80d3481d463f319680d6c1a83291b
    Author: Ian Beacall <ian@shogenapps.com>
    Date:   Tue Sep 16 03:50:02 2025 +0900
    
        Update feed_helper.js
    
    commit 011b42425d230b9b7fb20659c5fe95fef40cf8ff
    Author: Ian Beacall <ian@shogenapps.com>
    Date:   Tue Sep 16 03:12:20 2025 +0900
    
        Initial Feed Helper additions

---
+++ src/features/feed_helper/feed_helper_options.js
@@ -0,0 +1,18 @@
+/*
+Created By: Ian Beacall (Beacall-6)
+*/
+
+import { registerFeature, OptionType } from "../../core/options/options_registry";
+import { isNetworkFeed, isContributions } from "../../core/pageType";
+
+registerFeature({
+  name: "Feed Helper",
+  id: "feedHelper",
+  description:
+    "Adds buttons to the Network Feed and Contributions pages to check for anomalies, recent activity, and activity by new members and newly-badged members.",
+  category: "Other",
+  creators: [{ name: "Ian Beacall", wikitreeid: "Beacall-6" }],
+  contributors: [],
+  defaultValue: false,
+  pages: [isNetworkFeed, isContributions],
+});
