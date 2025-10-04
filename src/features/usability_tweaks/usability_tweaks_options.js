c8d193d270443583991283f398290ebb5d7c54e4
Ian Beacall <ian@shogenapps.com>
2025-10-04 17:02:23 +0900

    Update usability_tweaks_options.js

--- src/features/usability_tweaks/usability_tweaks_options.js
+++ src/features/usability_tweaks/usability_tweaks_options.js
@@ -209,7 +209,7 @@ const usabilityTweaks = {
         {
           id: "addScratchPadDownload",
           type: OptionType.CHECKBOX,
-          label: "Add a Download Scatch Pad button below the Scratch Pad.",
+          label: "Add a Download Scratch Pad button below the Scratch Pad.",
           defaultValue: true,
         },
       ],
