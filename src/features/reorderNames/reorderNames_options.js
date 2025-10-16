01df520eb4c66492566cc72c44a351b0ed594363
Ian Beacall <ian@shogenapps.com>
2025-10-16 14:59:03 +0900

    Update reorderNames_options.js

--- src/features/reorderNames/reorderNames_options.js
+++ src/features/reorderNames/reorderNames_options.js
@@ -8,10 +8,12 @@ import { isProfilePage } from "../../core/pageType";
 registerFeature({
   name: "ReorderNames",
   id: "reorderNames",
-  description: "",
+  description:
+    "Reorders non-Latin alphabet names; each language appears on its own line in the family box. " +
+    "Supports Chinese, Cyrillic, Greek, Hebrew, and Korean. Caveat: sometimes not all OLNs are displayed.",
   category: "Profile",
   creators: [{ name: "Elaine Martzen", wikitreeid: "Weatherall-96" }],
   contributors: [],
   defaultValue: false,
-  pages: [ isProfilePage],
+  pages: [isProfilePage],
 });
