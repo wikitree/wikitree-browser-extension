7ae23f78364c1275003712e5876f52f8062bdbe2
Ian Beacall <ian@shogenapps.com>
2025-10-23 00:20:54 +0900

    Enable Countdown feature on Project pages
    The Countdown feature is now available on both Space and Project pages by updating its page registration. Also, incremented extension version to 2.7.0.9 in both Chrome and Firefox manifests.

--- src/features/countdown/countdown_options.js
+++ src/features/countdown/countdown_options.js
@@ -3,7 +3,7 @@ Created By: Ian Beacall (Beacall-6)
 */
 
 import { registerFeature, OptionType } from "../../core/options/options_registry";
-import { isSpacePage } from "../../core/pageType";
+import { isSpacePage, isProjectPage } from "../../core/pageType";
 
 registerFeature({
   name: "Countdown",
@@ -13,7 +13,7 @@ registerFeature({
   creators: [{ name: "Ian Beacall", wikitreeid: "Beacall-6" }],
   contributors: [],
   defaultValue: true,
-  pages: [isSpacePage],
+  pages: [isSpacePage, isProjectPage],
   options: [
     {
       id: "updateFrequency",
