35d054313ec9ce4597339e9d2baf28666728ff64
Ian Beacall <ian@shogenapps.com>
2025-10-26 02:35:18 +0900

    Update auto_bio.js

--- src/features/auto_bio/auto_bio.js
+++ src/features/auto_bio/auto_bio.js
@@ -5167,7 +5167,7 @@ export function sourcesArray(bio) {
       const baptismDateMatch = aRef.Text.match(/familysearch.*,.*?baptis.*?\bon\b (.*?\d{4}\b)/i);
       const baptismDateMatch2 = aRef.Text.match(/familysearch.*\),.*(\b(\d{1,2}\s)(\w{3}\s)\d{4}\b);/i);
       const birthDateMatch = aRef.Text.match(/familysearch.*,.*?\bborn\b (.*?\d{4}\b)/i);
-      const baptismLocationMatch = aRef.Text.match(/familysearch.*,.*?\bin|at\b (.*?)\./i);
+      const baptismLocationMatch = aRef.Text.match(/familysearch.*,.*?\b(?:in|at)\b (.*?)\./i);
       const baptismLocationMatch2 = aRef.Text.match(
         /familysearch.*\),.*\b\d{1,2}\s\w{3}\s\d{4}\b;.*Baptism,\s(.*), (United Kingdom|USA|United States|Canada|Australia|New Zealand)/i
       );
