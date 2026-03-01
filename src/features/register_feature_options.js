1714a892e76dfb08c3f66dd006385fea5027d8bf
Ian Beacall <ian@shogenapps.com>
2026-03-01 12:53:26 +0900

    Add Space Style feature and bump version
    Introduce a new Space Style feature for space pages that can remove the right sidebar and move the profile manager into the main content area. Adds feature files: JS (behavior + toggle), CSS (styles for fullwidth and toggle), and options registration (with 'alwaysWide' and 'wideIfComment' checkboxes). Registers the feature in the feature options bundle and imports it from content.js. Also increments extension version to 2.11.1.11 in Chrome and Firefox manifests.

--- src/features/register_feature_options.js
+++ src/features/register_feature_options.js
@@ -118,6 +118,7 @@ import "./sort_theme_people/sort_theme_people_options";
 import "./sortBadges/sortBadges_options";
 import "./sourcepreview/sourcepreview_options";
 import "./space_drafts/space_drafts_options";
+import "./space_style/space_style_options";
 import "./space_watchlist_sorter/space_watchlist_sorter_options";
 import "./spacepreview/spacepreview_options";
 import "./sticky_header/sticky_header_options";
